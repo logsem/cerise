5f7fa580f2407b6e3f9b5865643ce56d3e80ad64
Aina Linn Georges <aina.georges@egmail.com>
2019-10-16 09:51:44 +0200

    progress towards store

--- theories/ftlr/Store.v
+++ theories/ftlr/Store.v
@@ -3,7 +3,6 @@ From iris.program_logic Require Import weakestpre adequacy lifting.
 From stdpp Require Import base.
 From cap_machine Require Export logrel.
 
-
 Section fundamental.
   Context `{memG Σ, regG Σ, STSG Σ, logrel_na_invs Σ,
             MonRef: MonRefG (leibnizO _) CapR_rtc Σ,
@@ -62,77 +61,100 @@ Section fundamental.
     intros Hp Hsome i Hbae Hfp HO Hi.
     iIntros "#IH #Hbe #Hreg #Harel #Hmono #Hw".
     iIntros "Hfull Hna Hr Ha HPC Hmap".
-    Admitted. 
-
-  (*
-  Lemma RX_Store_case:
-    ∀ E0 r a g fs fr b e p' w dst src
-      (* RWX case *)
-      (fundamental_RWX : ∀ stsf E r b e g a,
-          ((∃ p, ⌜PermFlows RWX p⌝ ∧
-                 ([∗ list] a ∈ (region_addrs b e), na_inv logrel_nais (logN .@ a)
-                                      (read_write_cond a p interp))) →
-           ⟦ inr ((RWX,g),b,e,a) ⟧ₑ stsf E r)%I)
-      (* RWLX case *)
-      (fundamental_RWLX : ∀ stsf E r b e g a,
-          ((∃ p, ⌜PermFlows RWLX p⌝ ∧
-                 ([∗ list] a ∈ (region_addrs b e), na_inv logrel_nais (logN .@ a)
-                                      (read_write_cond a p interp))) →
-           ⟦ inr ((RWLX,g),b,e,a) ⟧ₑ stsf E r)%I)
-      (Hreach : ∀ a' : Addr, (b <= a')%a ∧ (a' <= e)%a → ↑logN.@a' ⊆ E0)
-      (H3 : ∀ x : RegName, (λ x0 : RegName, is_Some (r !! x0)) x)
-      (i : isCorrectPC (inr (RX, g, b, e, a)))
-      (Hbae : (b <= a)%a ∧ (a <= e)%a)
-      (Hfp : PermFlows RX p')
-      (Hi : cap_lang.decode w = cap_lang.Store dst src),
-      □ ▷ (∀ a0 a1 a2 a3 a4 a5 a6,
-              full_map a0
-           -∗ (∀ r0, ⌜r0 ≠ PC⌝ → (((fixpoint interp1) E0) (a3, a4)) (a0 !r! r0))
-           -∗ registers_mapsto (<[PC:=inr (RX, a2, a5, a6, a1)]> a0)
-           -∗ sts_full a3 a4
-           -∗ na_own logrel_nais E0
-           -∗ □ (∃ p, ⌜PermFlows RX p⌝
-                      ∧ ([∗ list] a7 ∈ region_addrs a5 a6, 
-                         na_inv logrel_nais (logN.@a7)
-                                (∃ w0 : leibnizO Word,
-                                    a7 ↦ₐ[p] w0
-                                  ∗ (∀ stsf E1, ▷ ((interp E1) stsf) w0))))
-           -∗ □ ⌜∀ a' : Addr, (a5 ≤ a')%Z ∧ (a' ≤ a6)%Z → ↑logN.@a' ⊆ E0⌝
-           -∗ ⟦ [a3, a4, E0] ⟧ₒ)
-        -∗ □ ([∗ list] a0 ∈ region_addrs b e, na_inv logrel_nais (logN.@a0)
-                    (∃ w0, a0 ↦ₐ[p'] w0 ∗ (∀ stsf E1, ▷ ((interp E1) stsf) w0)))
-        -∗ □ (∀ r0 : RegName, ⌜r0 ≠ PC⌝ → (((fixpoint interp1) E0) (fs, fr)) (r !r! r0))
-        -∗ □ na_inv logrel_nais (logN.@a)
-              (∃ w0 : leibnizO Word, a ↦ₐ[p'] w0 ∗ (∀ stsf E1, ▷ ((interp E1) stsf) w0))
-        -∗ □ ▷ (∀ stsf E1, ▷ ((interp E1) stsf) w)
-        -∗ □ ▷ ▷ ((interp E0) (fs, fr)) w
-        -∗ sts_full fs fr
-        -∗ a ↦ₐ[p'] w
-        -∗ na_own logrel_nais (E0 ∖ ↑logN.@a)
-        -∗ (▷ (∃ w0, a ↦ₐ[p'] w0 ∗ (∀ stsf E1, ▷ ((interp E1) stsf) w0))
-              ∗ na_own logrel_nais (E0 ∖ ↑logN.@a)
-            ={⊤}=∗ na_own logrel_nais E0)
-        -∗ PC ↦ᵣ inr (RX, g, b, e, a)
-        -∗ ([∗ map] k↦y ∈ delete PC
-                    (<[PC:=inr (RX, g, b, e, a)]> r), 
-            k ↦ᵣ y)
-        -∗ WP Instr Executable
-           {{ v, WP fill [SeqCtx] (of_val v)
-                    {{ v0, ⌜v0 = HaltedV⌝ → ∃ r0 fs' fr',
-                           full_map r0 ∧ registers_mapsto r0
-                           ∗ ⌜related_sts_priv fs fs' fr fr'⌝
-                           ∗ na_own logrel_nais E0
-                           ∗ sts_full fs' fr' }} }}.
-  Proof.
-    intros E0 r a g fs fr b e p' w. intros.
-    iIntros "#IH #Hinv #Hreg #Hinva #Hval #Hval'".
-    iIntros "Hsts Ha Hown Hcls HPC Hmap".
     rewrite delete_insert_delete.
+    (* for the successful writes from PC, we want to show that 
+         the region can be closed again *)
+    iAssert (((fixpoint interp1) (fs, fr)) (inr ((p,g),b,e,a)))%I
+        as "#HPCw".
+    { rewrite (fixpoint_interp1_eq _ (inr _)) /=. 
+      destruct Hp as [-> | [-> | ->] ];
+      (iExists _,_,_,_; iSplitR;[eauto|];
+       iExists p';do 2 (iSplit; auto);
+       iAlways;iIntros (a' r' W' Hin) "Hfuture";
+       iNext; destruct W' as [fs' fr']; 
+       iExists _,_; do 2 (iSplitR; [auto|]); 
+       iIntros "(#[Hfull Hreg'] & Hmap & Hr & Hsts & Hna) /="; 
+       iExists _,_,_,_,_; iSplit;[eauto|];
+       iApply ("IH" with "Hfull Hreg' Hmap Hr Hsts Hna"); auto).
+    }
     destruct (reg_eq_dec dst PC).
-    * subst dst. assert (writeAllowed RX = false) by reflexivity.
-      iApply (wp_store_fail1' with "[HPC Ha]"); eauto; iFrame.
-      iNext. iIntros. iApply wp_pure_step_later; auto.
-      iNext. iApply wp_value. iIntros. discriminate.
+    - subst dst.
+       destruct Hp as [-> | Hp].
+       { (* if p is RX, write is not allowed *)
+         iApply (wp_store_fail1' with "[$HPC $Ha]"); eauto.
+         iNext. iIntros (_).
+         iApply wp_pure_step_later; auto. iNext.
+         iApply wp_value. iIntros. discriminate. }
+      destruct src.
+      + (* successful write into a *) admit.
+      + destruct (Hsome r0) as [wdst Hsomedst].
+        destruct (reg_eq_dec r0 PC).
+        * simplify_eq.
+          destruct Hp as [-> | ->].
+          ** destruct (isLocalWord wdst) eqn:Hlocal.
+             *** (* failure: trying to write a local word without perm *)
+               admit.
+             *** (* successful write into a: word is not local *)
+               admit.
+          ** (* successful write into a: perm is local allowed *)
+            admit. 
+        * iDestruct ((big_sepM_delete _ _ r0) with "Hmap")
+            as "[Hdst Hmap]"; [rewrite lookup_delete_ne; eauto|].
+          destruct Hp as [-> | ->]. 
+          ** destruct (isLocalWord wdst) eqn:Hlocal.
+             *** (* failure: trying to write a local word without perm *)
+               admit.
+             *** (* successful write into a: word is not local *)
+               admit.
+          ** (* successful write into a: perm is local allowed *)
+            admit.
+    - destruct (Hsome dst) as [wdst Hsomedst].
+      iDestruct ((big_sepM_delete _ _ dst) with "Hmap")
+        as "[Hdst Hmap]"; [rewrite lookup_delete_ne; eauto|].
+      destruct wdst.
+      { (* if dst does not contain cap, fail *)
+        iApply (wp_store_fail2 with "[HPC Hdst Ha]"); eauto; iFrame.
+        iNext. iIntros. iApply wp_pure_step_later; auto.
+        iNext. iApply wp_value. iIntros. discriminate. }
+      destruct c,p0,p0,p0.
+      case_eq (writeAllowed p
+               && withinBounds (p0,l,a2,a1,a0));  
+        intros Hconds.
+      + destruct src.
+        * (* successful write into a0 *)
+          admit.
+        * destruct (reg_eq_dec PC r0),(reg_eq_dec dst r0);
+            first congruence; subst. 
+          ** (* successful store of PC into a0 *)
+            admit.
+          ** case_eq (negb (isLocal l) || match p with
+                                         | RWL | RWLX => true
+                                         | _ => false end); intros Hconds'. 
+             *** (* successful write from r0 into a0 *)
+               admit.
+             *** (* locality failure *)
+               admit.
+          ** destruct (Hsome r0) as [wsrc Hsomesrc].
+             assert (delete PC r !! r0 = Some wsrc).
+             { rewrite lookup_delete_ne; auto. }
+             iDestruct ((big_sepM_delete _ _ r0) with "Hmap")
+               as "[Hsrc Hmap]"; [rewrite lookup_delete_ne; eauto|].
+             destruct wsrc.
+             *** (* successful write from r0 into a0 *)
+               admit.
+             *** case_eq (negb (isLocalWord (inr c)) || match p0 with
+                                                       | RWL | RWLX => true
+                                                       | _ => false end); intros Hconds'. 
+                 **** (* successful write from r0 into a0 *)
+                   admit.
+             *** (* locality failure *)
+               admit.
+             
+      destruct (writeAllowed p).
+      + iApply (wp_store_success_reg_same with "[$HPC $Ha]"). 
+        iApply (wp_store_fail1' with "[$HPC $Ha]"); eauto. 
+        iNext. iIntros. iApply wp_pure_step_later; auto.
+        iNext. iApply wp_value. iIntros. discriminate.
     * destruct (H3 dst) as [wdst Hsomedst].
       iDestruct ((big_sepM_delete _ _ dst) with "Hmap") as "[Hdst Hmap]"; [rewrite lookup_delete_ne; eauto|].
       destruct wdst.
