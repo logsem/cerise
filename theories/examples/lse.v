logsem/cerise

commit 30b7976b36d74197171a907156a679589e84b149

Move the helper ltacs used in various files to macros_helpers.v

theories/examples/lse.v
@@ -73,52 +73,6 @@ Section roe.
           {nainv: logrel_na_invs Σ}
           `{MP: MachineParameters}.
 
-  (* -------------------------------- LTACS -------------------------------------- *)
-  
-  (* Tactic for destructing a list into elements *)
-  Ltac destruct_list l :=
-    match goal with
-    | H : strings.length l = _ |- _ =>
-      let a := fresh "a" in
-      let l' := fresh "l" in
-      destruct l as [|a l']; [inversion H|];
-      repeat (let a' := fresh "a" in
-              destruct l' as [|a' l'];[by inversion H|]);
-      destruct l'; [|by inversion H]
-    end.
-
-  Ltac iPrologue_pre :=
-    match goal with
-    | Hlen : strings.length ?a = ?n |- _ =>
-      let a' := fresh "a" in
-      destruct a as [| a' a]; [inversion Hlen|]
-    end.
-
-  Ltac iPrologue prog :=
-    iDestruct prog as "[Hi Hprog]";
-    iApply (wp_bind (fill [SeqCtx])).
-
-  Ltac iEpilogue prog :=
-    iNext; iIntros prog; iSimpl;
-    iApply wp_pure_step_later;auto;iNext.
-
-  Ltac middle_lt prev index :=
-    match goal with
-    | Ha_first : ?a !! 0 = Some ?a_first |- _
-    => apply Z.lt_trans with prev; auto; apply incr_list_lt_succ with a index; auto
-    end.
-
-  Ltac iCorrectPC i j :=
-    eapply isCorrectPC_contiguous_range with (a0 := i) (an := j); eauto; [];
-    cbn; solve [ repeat constructor ].
-
-  Ltac iContiguous_next Ha index :=
-    apply contiguous_of_contiguous_between in Ha;
-    generalize (contiguous_spec _ Ha index); auto.
-
-  Ltac consider_next_reg r1 r2 :=
-    destruct (decide (r1 = r2));[subst;rewrite lookup_insert;eauto|rewrite lookup_insert_ne;auto]. 
-
   (* ----------------------------------------------------------------------------- *)
   Definition ro := encodePerm RO.
   Definition roe_instrs f_m f_a r1 :=

theories/examples/addr_reg_sample.v
@@ -86,6 +86,7 @@ Section instr_encodings.
   Definition sub_z_r r1 z r2 := encodeInstrW (Sub r1 (inl z) (inr r2)).
   Definition sub_z_z r z1 z2 := encodeInstrW (Sub r (inl z1) (inl z2)).
   Definition subseg_r_r r1 r2 r3 := encodeInstrW (Subseg r1 (inr r2) (inr r3)).
+  Definition subseg_z_z r1 z1 z2 := encodeInstrW (Subseg r1 (inl z1) (inl z2)).
   Definition jnz r1 r2 := encodeInstrW (Jnz r1 r2).
   Definition lt_r_r r1 r2 r3 := encodeInstrW (Lt r1 (inr r2) (inr r3)).
   Definition lt_z_r r1 z r2 := encodeInstrW (Lt r1 (inl z) (inr r2)).

theories/examples/call.v
@@ -203,61 +203,6 @@ Section call.
             (let Hneq := fresh "Hneq" in destruct Hne as [Hneq Hne])); auto.
 
 
-  (* -------------------------------- LTACS ------------------------------------------- *)
-  
-  (* Tactic for destructing a list into elements *)
-  Ltac destruct_list l :=
-    match goal with
-    | H : strings.length l = _ |- _ =>
-      let a := fresh "a" in
-      let l' := fresh "l" in
-      destruct l as [|a l']; [inversion H|];
-      repeat (let a' := fresh "a" in
-              destruct l' as [|a' l'];[by inversion H|]);
-      destruct l'; [|by inversion H]
-    end.
-
-  Ltac iPrologue_pre :=
-    match goal with
-    | Hlen : length ?a = ?n |- _ =>
-      let a' := fresh "a" in
-      destruct a as [| a' a]; inversion Hlen; simpl
-    end.
-
-  Ltac iPrologue prog :=
-    (try iPrologue_pre);
-    iDestruct prog as "[Hi Hprog]";
-    iApply (wp_bind (fill [SeqCtx])).
-
-  Ltac iEpilogue prog :=
-    iNext; iIntros prog; iSimpl;
-    iApply wp_pure_step_later;auto;iNext.
-
-  Ltac middle_lt prev index :=
-    match goal with
-    | Ha_first : ?a !! 0 = Some ?a_first |- _
-    => apply Z.lt_trans with prev; auto; apply incr_list_lt_succ with a index; auto
-    end.
-
-  Ltac iCorrectPC i j :=
-    eapply isCorrectPC_contiguous_range with (a0 := i) (an := j); eauto; [];
-    cbn; solve [ repeat constructor ].
-
-  Ltac iContiguous_next Ha index :=
-    apply contiguous_of_contiguous_between in Ha;
-    generalize (contiguous_spec _ Ha index); auto.
-
-  Ltac disjoint_from_rmap rmap :=
-    match goal with
-    | Hsub : _ ⊆ dom (gset RegName) rmap |- _ !! ?r = _ => 
-      assert (is_Some (rmap !! r)) as [x Hx];[apply elem_of_gmap_dom;apply Hsub;constructor|];
-      apply map_disjoint_Some_l with rmap x;auto;apply map_disjoint_union_r_2;auto
-    end.
-  
-  Ltac iContiguous_le Ha index :=
-    eapply contiguous_between_middle_bounds with (i:=index) in Ha;eauto;clear -Ha;solve_addr.
-  
-
   Lemma store_locals_spec_middle
         (* cont *) φ
         (* list of locals *) r1 locals mlocals wsr

theories/examples/callback.v
@@ -62,60 +62,6 @@ Section callback.
           {nainv: logrel_na_invs Σ}
           `{MP: MachineParameters}.
 
-  (* -------------------------------- LTACS ------------------------------------------- *)
-  
-  (* Tactic for destructing a list into elements *)
-  Ltac destruct_list l :=
-    match goal with
-    | H : strings.length l = _ |- _ =>
-      let a := fresh "a" in
-      let l' := fresh "l" in
-      destruct l as [|a l']; [inversion H|];
-      repeat (let a' := fresh "a" in
-              destruct l' as [|a' l'];[by inversion H|]);
-      destruct l'; [|by inversion H]
-    end.
-
-  Ltac iPrologue_pre :=
-    match goal with
-    | Hlen : length ?a = ?n |- _ =>
-      let a' := fresh "a" in
-      destruct a as [| a' a]; inversion Hlen; simpl
-    end.
-
-  Ltac iPrologue prog :=
-    (try iPrologue_pre);
-    iDestruct prog as "[Hi Hprog]";
-    iApply (wp_bind (fill [SeqCtx])).
-
-  Ltac iEpilogue prog :=
-    iNext; iIntros prog; iSimpl;
-    iApply wp_pure_step_later;auto;iNext.
-
-  Ltac middle_lt prev index :=
-    match goal with
-    | Ha_first : ?a !! 0 = Some ?a_first |- _
-    => apply Z.lt_trans with prev; auto; apply incr_list_lt_succ with a index; auto
-    end.
-
-  Ltac iCorrectPC i j :=
-    eapply isCorrectPC_contiguous_range with (a0 := i) (an := j); eauto; [];
-    cbn; solve [ repeat constructor ].
-
-  Ltac iContiguous_next Ha index :=
-    apply contiguous_of_contiguous_between in Ha;
-    generalize (contiguous_spec _ Ha index); auto.
-
-  Ltac disjoint_from_rmap rmap :=
-    match goal with
-    | Hsub : _ ⊆ dom (gset RegName) rmap |- _ !! ?r = _ => 
-      assert (is_Some (rmap !! r)) as [x Hx];[apply elem_of_gmap_dom;apply Hsub;constructor|];
-      apply map_disjoint_Some_l with rmap x;auto;apply map_disjoint_union_r_2;auto
-    end.
-  
-  Ltac iContiguous_le Ha index :=
-    eapply contiguous_between_middle_bounds with (i:=index) in Ha;eauto;clear -Ha;solve_addr.
-
   (* ---------------------------------------------------------------------------------- *)
   
   Fixpoint restore_locals_instrs r1 locals :=

theories/examples/counter.v
@@ -8,53 +8,6 @@ Section counter.
           {nainv: logrel_na_invs Σ}
           `{MP: MachineParameters}.
 
-  (* -------------------------------- LTACS -------------------------------------- *)
-  
-  (* Tactic for destructing a list into elements *)
-  Ltac destruct_list l :=
-    match goal with
-    | H : strings.length l = _ |- _ =>
-      let a := fresh "a" in
-      let l' := fresh "l" in
-      destruct l as [|a l']; [inversion H|];
-      repeat (let a' := fresh "a" in
-              destruct l' as [|a' l'];[by inversion H|]);
-      destruct l'; [|by inversion H]
-    end.
-
-  Ltac iPrologue_pre :=
-    match goal with
-    | Hlen : length ?a = ?n |- _ =>
-      let a' := fresh "a" in
-      destruct a as [| a' a]; inversion Hlen; simpl
-    end.
-
-  Ltac iPrologue prog :=
-    (try iPrologue_pre);
-    iDestruct prog as "[Hi Hprog]";
-    iApply (wp_bind (fill [SeqCtx])).
-
-  Ltac iEpilogue prog :=
-    iNext; iIntros prog; iSimpl;
-    iApply wp_pure_step_later;auto;iNext.
-
-  Ltac middle_lt prev index :=
-    match goal with
-    | Ha_first : ?a !! 0 = Some ?a_first |- _
-    => apply Z.lt_trans with prev; auto; apply incr_list_lt_succ with a index; auto
-    end.
-
-  Ltac iCorrectPC i j :=
-    eapply isCorrectPC_contiguous_range with (a0 := i) (an := j); eauto; [];
-    cbn; solve [ repeat constructor ].
-
-  Ltac iContiguous_next Ha index :=
-    apply contiguous_of_contiguous_between in Ha;
-    generalize (contiguous_spec _ Ha index); auto.
-
-  Ltac consider_next_reg r1 r2 :=
-    destruct (decide (r1 = r2));[subst;rewrite lookup_insert;eauto|rewrite lookup_insert_ne;auto]. 
-
   (* ----------------------------------------------------------------------------- *)
   Definition r_ret := r_t31.
   

theories/examples/counter_preamble.v
@@ -3,7 +3,7 @@ From iris.proofmode Require Import tactics.
 From iris.base_logic Require Import invariants.
 Require Import Eqdep_dec.
 From cap_machine Require Import rules logrel fundamental. 
-From cap_machine.examples Require Import macros malloc counter.
+From cap_machine.examples Require Import macros macros_helpers malloc counter.
 From stdpp Require Import countable.
 
 Section counter_example_preamble.
@@ -104,26 +104,6 @@ Section counter_example_preamble.
     iDestruct prog as "[Hi Hprog]";
     iApply (wp_bind (fill [SeqCtx])).
 
-  Ltac iEpilogue prog :=
-    iNext; iIntros prog; iSimpl;
-    iApply wp_pure_step_later;auto;iNext.
-
-  Ltac middle_lt prev index :=
-                                   match goal with
-    | Ha_first : ?a !! 0 = Some ?a_first |- _
-    => apply Z.lt_trans with prev; auto; apply incr_list_lt_succ with a index; auto
-    end.
-
-  Ltac iCorrectPC i j :=
-    eapply isCorrectPC_contiguous_range with (a0 := i) (an := j); eauto; [];
-    cbn; solve [ repeat constructor ].
-
-  Ltac iContiguous_next Ha index :=
-    apply contiguous_of_contiguous_between in Ha;
-    generalize (contiguous_spec _ Ha index); auto.
-
-
-                                                  
   (* The following three lemmas show that the created closures are valid *)
   Lemma incr_closure_valid incr_prog restc count_incrN countN count_clsN b_cls e_cls b_cls' e_cls' b_cls'' e_cls''
         pc_p pc_b pc_e counter_first counter_end linkc linkc' b_cell e_cell :

theories/examples/macros.v
@@ -10,37 +10,6 @@ Section macros.
           `{MP: MachineParameters}.
   
 
-  (* -------------------------------- LTACS ------------------------------------------- *)
-  Ltac iPrologue_pre :=
-    match goal with
-    | Hlen : length ?a = ?n |- _ =>
-      let a' := fresh "a" in
-      destruct a as [| a' a]; inversion Hlen; simpl
-    end.
-
-  Ltac iPrologue prog :=
-    (try iPrologue_pre);
-    iDestruct prog as "[Hi Hprog]";
-    iApply (wp_bind (fill [SeqCtx])).
-
-  Ltac iEpilogue prog :=
-    iNext; iIntros prog; iSimpl;
-    iApply wp_pure_step_later;auto;iNext.
-
-  Ltac middle_lt prev index :=
-    match goal with
-    | Ha_first : ?a !! 0 = Some ?a_first |- _
-    => apply Z.lt_trans with prev; auto; apply incr_list_lt_succ with a index; auto
-    end.
-
-  Ltac iCorrectPC i j :=
-    eapply isCorrectPC_contiguous_range with (a0 := i) (an := j); eauto; [];
-    cbn; solve [ repeat constructor ].
-
-  Ltac iContiguous_next Ha index :=
-    apply contiguous_of_contiguous_between in Ha;
-    generalize (contiguous_spec _ Ha index); auto.
-
   (* --------------------------------------------------------------------------------- *)
   (* ------------------------------------- FETCH ------------------------------------- *)
   (* --------------------------------------------------------------------------------- *)
@@ -1612,6 +1581,9 @@ Section macros.
   Definition v5 := encodeInstr (Load r_t1 r_t1).
   Definition v6 := encodeInstr (Jmp r_t1).
 
+  Definition activation_instrs wcode wenv : list Word :=
+    [ inl v1; inl v2; inl v3; inl v4; inl v5; inl v6; wcode; wenv ].
+
   (* scrtcls: "static" crtcls. Requires a capability for the memory region where to write
      the activation record.
 
@@ -1672,7 +1644,7 @@ Section macros.
     (* continuation *)
     ∗ ▷ (PC ↦ᵣ inr (pc_p,pc_b,pc_e,a_last) ∗ scrtcls rcode rdata a
          ∗ r_t1 ↦ᵣ inr (E, act_b, act_e, act_b)
-         ∗ [[act_b,act_e]] ↦ₐ [[ [inl v1;inl v2;inl v3;inl v4;inl v5;inl v6;wcode;wvar] ]]
+         ∗ [[act_b,act_e]] ↦ₐ [[ activation_instrs wcode wvar ]]
          ∗ rcode ↦ᵣ inl 0%Z
          ∗ rdata ↦ᵣ inl 0%Z
          -∗ WP Seq (Instr Executable) {{ φ }})
@@ -1886,7 +1858,7 @@ Section macros.
          ∗ a_entry ↦ₐ inr (E,b_m,e_m,b_m)
          (* the newly allocated region *)
          ∗ (∃ (b e : Addr), ⌜(b + 8)%a = Some e⌝ ∧ r_t1 ↦ᵣ inr (E,b,e,b)
-         ∗ [[b,e]] ↦ₐ [[ [inl v1;inl v2;inl v3;inl v4;inl v5;inl v6;wcode;wvar] ]]
+         ∗ [[b,e]] ↦ₐ [[ activation_instrs wcode wvar ]]
          ∗ r_t0 ↦ᵣ cont
          ∗ r_t2 ↦ᵣ inl 0%Z
          ∗ na_own logrel_nais EN
@@ -2001,11 +1973,11 @@ Section macros.
     PC ↦ᵣ inr (pc_p, b_cls, e_cls, b_cls)
     ∗ r_t1 ↦ᵣ r1v
     ∗ r_env ↦ᵣ renvv
-    ∗ [[b_cls, e_cls]]↦ₐ[[ [inl v1; inl v2; inl v3; inl v4; inl v5; inl v6; wcode; wenv] ]]
+    ∗ [[b_cls, e_cls]]↦ₐ[[ activation_instrs wcode wenv ]]
     ∗ (  PC ↦ᵣ updatePcPerm wcode
        ∗ r_t1 ↦ᵣ wcode
        ∗ r_env ↦ᵣ wenv
-       ∗ [[b_cls, e_cls]]↦ₐ[[ [inl v1; inl v2; inl v3; inl v4; inl v5; inl v6; wcode; wenv] ]]
+       ∗ [[b_cls, e_cls]]↦ₐ[[ activation_instrs wcode wenv ]]
        -∗ WP Seq (Instr Executable) {{ φ }})
     ⊢
       WP Seq (Instr Executable) {{ φ }}.

theories/examples/macros_helpers.v
@@ -51,3 +51,60 @@ Section helpers.
   Qed.
 
 End helpers.
+
+(* Ltacs *)
+
+(* Tactic for destructing a list into elements *)
+Ltac destruct_list l :=
+  match goal with
+  | H : strings.length l = _ |- _ =>
+    let a := fresh "a" in
+    let l' := fresh "l" in
+    destruct l as [|a l']; [inversion H|];
+    repeat (let a' := fresh "a" in
+            destruct l' as [|a' l'];[by inversion H|]);
+    destruct l'; [|by inversion H]
+  end.
+
+Ltac iPrologue_pre :=
+  match goal with
+  | Hlen : length ?a = ?n |- _ =>
+    let a' := fresh "a" in
+    destruct a as [| a' a]; inversion Hlen; simpl
+  end.
+
+Ltac iPrologue prog :=
+  (try iPrologue_pre);
+  iDestruct prog as "[Hi Hprog]";
+  iApply (wp_bind (fill [SeqCtx])).
+
+Ltac iEpilogue prog :=
+  iNext; iIntros prog; iSimpl;
+  iApply wp_pure_step_later;auto;iNext.
+
+Ltac middle_lt prev index :=
+  match goal with
+  | Ha_first : ?a !! 0 = Some ?a_first |- _
+  => apply Z.lt_trans with prev; auto; apply incr_list_lt_succ with a index; auto
+  end.
+
+Ltac iCorrectPC i j :=
+  eapply isCorrectPC_contiguous_range with (a0 := i) (an := j); eauto; [];
+  cbn; solve [ repeat constructor ].
+
+Ltac iContiguous_next Ha index :=
+  apply contiguous_of_contiguous_between in Ha;
+  generalize (contiguous_spec _ Ha index); auto.
+
+Ltac disjoint_from_rmap rmap :=
+  match goal with
+  | Hsub : _ ⊆ dom (gset RegName) rmap |- _ !! ?r = _ =>
+    assert (is_Some (rmap !! r)) as [x Hx];[apply elem_of_gmap_dom;apply Hsub;constructor|];
+    apply map_disjoint_Some_l with rmap x;auto;apply map_disjoint_union_r_2;auto
+  end.
+
+Ltac iContiguous_le Ha index :=
+  eapply contiguous_between_middle_bounds with (i:=index) in Ha;eauto;clear -Ha;solve_addr.
+
+Ltac consider_next_reg r1 r2 :=
+  destruct (decide (r1 = r2));[subst;rewrite lookup_insert;eauto|rewrite lookup_insert_ne;auto].

commit ef3a4b829a41715da3e136d471ec7076199ffe9b

Fix lse.v that got broken by the refactoring in 30b7976b36

theories/examples/lse.v
@@ -73,6 +73,10 @@ Section roe.
           {nainv: logrel_na_invs Σ}
           `{MP: MachineParameters}.
 
+  Ltac iPrologue prog :=
+    iDestruct prog as "[Hi Hprog]";
+    iApply (wp_bind (fill [SeqCtx])).
+
   (* ----------------------------------------------------------------------------- *)
   Definition ro := encodePerm RO.
   Definition roe_instrs f_m f_a r1 :=