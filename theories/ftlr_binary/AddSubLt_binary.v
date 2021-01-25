3e10723abb142669f61e19b1b3a7c230dce39812
Alix Trieu <alix.trieu@cs.au.dk>
2021-01-25 16:35:47 +0100

    Add forgotten files

---
+++ theories/ftlr_binary/AddSubLt_binary.v
@@ -0,0 +1,97 @@
+From cap_machine Require Export logrel.
+From iris.proofmode Require Import tactics.
+From iris.program_logic Require Import weakestpre adequacy lifting.
+From stdpp Require Import base.
+From cap_machine Require Import ftlr_base_binary.
+From cap_machine.rules_binary Require Import rules_binary_base rules_binary_AddSubLt.
+
+Section fundamental.
+  Context {Σ:gFunctors} {memg:memG Σ} {regg:regG Σ}
+          {nainv: logrel_na_invs Σ} {cfgsg: cfgSG Σ}
+          `{MachineParameters}.
+
+  Notation D := ((prodO (leibnizO Word) (leibnizO Word)) -n> iPropO Σ).
+  Notation R := ((prodO (leibnizO Reg) (leibnizO Reg)) -n> iPropO Σ).
+  Implicit Types ww : (prodO (leibnizO Word) (leibnizO Word)).
+  Implicit Types w : (leibnizO Word).
+  Implicit Types interp : (D).
+
+  Lemma AddSubLt_spec_determ r i dst arg1 arg2 regs regs' retv retv' :
+    is_AddSubLt i dst arg1 arg2 ->
+    AddSubLt_spec i r dst arg1 arg2 regs retv ->
+    AddSubLt_spec i r dst arg1 arg2 regs' retv' ->
+    (regs = regs' ∨ retv = FailedV) ∧ retv = retv'.
+  Proof.
+    intros isASL Hspec1 Hspec2.
+    inversion Hspec1; inversion Hspec2; subst; simplify_eq; split; auto.
+    - inv H4; congruence.
+    - inv H4; congruence.
+    - inv H0; congruence.
+  Qed.
+
+  Lemma add_sub_lt_case i (r : prodO (leibnizO Reg) (leibnizO Reg)) (p : Perm)
+        (b e a : Addr) (w w' : Word) (dst : RegName) (arg1 arg2 : Z + RegName) (P : D):
+    is_AddSubLt i dst arg1 arg2 ->
+    ftlr_instr r p b e a w w' i P.
+  Proof.
+    intros HisASL Hp Hsome HisCorrect Hbae Hi.
+    iIntros "#IH #Hspec #Hinv #Hreg #Hinva #Hread Hsmap Hown Hs Ha Ha' HP Hcls HPC Hmap".
+    rewrite delete_insert_delete.
+    iDestruct ((big_sepM_delete _ _ PC) with "[HPC Hmap]") as "Hmap /=";
+      [apply lookup_insert|rewrite delete_insert_delete;iFrame|]. simpl.
+    iApply (wp_AddSubLt with "[$Ha $Hmap]"); eauto.
+    { simplify_map_eq. reflexivity. }
+    { rewrite /subseteq /map_subseteq /set_subseteq. intros rr _.
+      apply elem_of_gmap_dom. apply lookup_insert_is_Some'; eauto. destruct Hsome with rr; eauto. }
+    iIntros "!>" (regs' retv). iDestruct 1 as (HSpec) "[Ha Hmap]".
+
+    (* we assert that w = w' *)
+    iAssert (⌜w = w'⌝)%I as %Heqw.
+    { iDestruct "Hread" as "[Hread _]". iSpecialize ("Hread" with "HP"). by iApply interp_eq. }
+    destruct r as [r1 r2]. simpl in *.
+    iDestruct (interp_reg_eq r1 r2 (inr (p, b, e, a)) with "[]") as %Heq;[iSplit;auto|]. rewrite -!Heq.
+
+    iMod (step_AddSubLt _ [SeqCtx] i with "[$Ha' $Hsmap $Hs $Hspec]") as (retv' regs'') "(Hs' & Hs & Ha' & Hsmap) /=";[rewrite Heqw in Hi|..];eauto.
+    { rewrite lookup_insert. eauto. }
+    { rewrite /subseteq /map_subseteq /set_subseteq. intros rr _.
+      apply elem_of_gmap_dom. destruct (decide (PC = rr));[subst;rewrite lookup_insert;eauto|rewrite lookup_insert_ne //].
+      destruct Hsome with rr;eauto. }
+    { solve_ndisj. }
+    iDestruct "Hs'" as %HSpec'.
+
+    subst w'. rewrite Hi in HSpec, HSpec'.
+    specialize (AddSubLt_spec_determ _ i _ _ _ _ _ _ _ HisASL HSpec HSpec') as [Hregs <-].
+
+    destruct HSpec; cycle 1.
+    { iApply wp_pure_step_later; auto.
+      iMod ("Hcls" with "[Ha Ha' HP]"); [iExists w,w; iFrame|iModIntro]. iNext.
+      iApply wp_value; auto. iIntros; discriminate. }
+    { destruct Hregs as [<-|Hcontr];[|inversion Hcontr].
+      incrementPC_inv; simplify_map_eq.
+      iMod ("Hcls" with "[Ha Ha' HP]") as "_"; [iExists w,w; iFrame|iModIntro].
+      iApply wp_pure_step_later; auto. iNext.
+      iMod (do_step_pure _ [] with "[$Hspec $Hs]") as "Hs /=";auto.
+
+      destruct (decide (PC = dst));simplify_eq;simplify_map_eq.
+      - rewrite !insert_insert. rewrite lookup_insert in H2. inv H2.
+      - rewrite (insert_commute _ _ PC)// insert_insert.
+        iApply ("IH" $! ((<[dst:=_]> r1),(<[dst:=_]> r1)) with "[] [] Hmap Hsmap Hown Hs Hspec").
+        { iPureIntro. simpl. intros reg. destruct Hsome with reg;auto.
+          destruct (decide (dst = reg));[subst;rewrite lookup_insert|rewrite !lookup_insert_ne//];eauto. }
+        { simpl. iIntros (rr Hne). iDestruct ("Hreg" $! rr Hne) as "Hrr".
+          destruct (decide (rr = dst));[subst;rewrite /RegLocate;rewrite lookup_insert|].
+          - rewrite /interp !fixpoint_interp1_eq /=.
+            iPureIntro. reflexivity.
+          - rewrite /RegLocate !lookup_insert_ne//. revert Heq; rewrite map_eq' =>Heq.
+            destruct (r1 !! rr) eqn:Hsome';rewrite Hsome';[|rewrite !fixpoint_interp1_eq;eauto].
+            specialize (Heq rr w0). rewrite !lookup_insert_ne// in Heq. apply Heq in Hsome' as Heq'.
+            rewrite Heq'. auto.
+        }
+        { rewrite lookup_insert_ne// lookup_insert in H2. simplify_eq.
+          rewrite !fixpoint_interp1_eq /=. destruct Hp as [-> | ->];iDestruct "Hinv" as "[_ $]";auto. }
+    }
+  Qed.
+
+End fundamental.
+
+
---
+++ theories/rules_binary/rules_binary_IsPtr.v
@@ -0,0 +1,84 @@
+From cap_machine Require Export rules_binary_base rules_IsPtr.
+From iris.base_logic Require Export invariants gen_heap.
+From iris.program_logic Require Export weakestpre ectx_lifting.
+From iris.proofmode Require Import tactics.
+From iris.algebra Require Import frac.
+
+Section cap_lang_spec_rules.
+  Context `{cfgSG Σ, MachineParameters, invG Σ}.
+  Implicit Types P Q : iProp Σ.
+  Implicit Types σ : cap_lang.state.
+  Implicit Types a b : Addr.
+  Implicit Types r : RegName.
+  Implicit Types w : Word.
+  Implicit Types reg : gmap RegName Word.
+  Implicit Types ms : gmap Addr Word.
+
+  Lemma step_IsPtr Ep K pc_p pc_b pc_e pc_a w dst src regs :
+    decodeInstrW w = IsPtr dst src ->
+    isCorrectPC (inr (pc_p, pc_b, pc_e, pc_a)) →
+    regs !! PC = Some (inr (pc_p, pc_b, pc_e, pc_a)) →
+    regs_of (IsPtr dst src) ⊆ dom _ regs →
+
+    nclose specN ⊆ Ep →
+
+    spec_ctx ∗ ⤇ fill K (Instr Executable) ∗ pc_a ↣ₐ w ∗ ([∗ map] k↦y ∈ regs, k ↣ᵣ y)
+    ={Ep}=∗ ∃ retv regs', ⤇ fill K (of_val retv) ∗ ⌜ IsPtr_spec regs dst src regs' retv ⌝ ∗ pc_a ↣ₐ w ∗ ([∗ map] k↦y ∈ regs', k ↣ᵣ y).
+  Proof.
+    iIntros (Hinstr Hvpc HPC Dregs Hcls) "(#Hinv & Hj & Hpc_a & Hmap)".
+    iDestruct "Hinv" as (ρ) "Hinv". rewrite /spec_inv.
+    iInv specN as ">Hinv'" "Hclose". iDestruct "Hinv'" as (e [σr σm]) "[Hown %] /=".
+    iDestruct (regspec_heap_valid_inclSepM with "Hown Hmap") as %Hregs.
+    have HPC' := regs_lookup_eq _ _ _ HPC.
+    have Hx := lookup_weaken _ _ _ _ HPC Hregs.
+    iDestruct (spec_heap_valid with "[$Hown $Hpc_a]") as %Hpc_a.
+    iDestruct (spec_expr_valid with "[$Hown $Hj]") as %Heq; subst e.
+
+    specialize (normal_always_step (σr,σm)) as [c [ σ2 Hstep]].
+    eapply step_exec_inv in Hstep; eauto.
+
+    specialize (indom_regs_incl _ _ _ Dregs Hregs) as Hri. unfold regs_of in Hri.
+    destruct (Hri dst) as [wdst [H'dst Hdst]]. by set_solver+.
+
+    destruct (Hri src) as [wsrc [Hwsrc Hwsrc']]; [set_solver+|]. simpl in Hwsrc'.
+
+    assert ((c, σ2) = updatePC (update_reg (σr, σm) dst (match wsrc with inl _ => inl 0%Z | inr _ => inl 1%Z end))) as HH.
+    { rewrite -Hstep /= /RegLocate Hwsrc'.
+      destruct wsrc; reflexivity. }
+    rewrite /update_reg /= in HH.
+
+    destruct (incrementPC (<[ dst := (match wsrc with inl _ => inl 0%Z | inr _ => inl 1%Z end) ]> regs)) as [regs''|] eqn:Hregs';
+      pose proof Hregs' as H'regs'; cycle 1.
+    { apply incrementPC_fail_updatePC with (m:=σm) in Hregs'.
+      eapply updatePC_fail_incl with (m':=σm) in Hregs'.
+      2: by apply lookup_insert_is_Some'; eauto.
+      2: by apply insert_mono; eauto.
+      simplify_pair_eq.
+      iMod ((regspec_heap_update_inSepM _ _ _ dst (match wsrc with inl _ => inl 0%Z | inr _ => inl 1%Z end)) with "Hown Hmap") as "[Hown Hmap]"; eauto.
+      iExists FailedV,_. iMod (exprspec_mapsto_update _ _ (fill K (Instr Failed)) with "Hown Hj") as "[Hown Hj]".
+      iFrame.
+      iMod ("Hclose" with "[Hown]") as "_".
+      { iNext. iExists _,_;iFrame. iPureIntro. eapply rtc_r;eauto.
+        simpl. prim_step_from_exec.
+      }
+      iModIntro. iPureIntro. econstructor; eauto.
+      destruct wsrc; simpl in *; auto.
+    }
+
+    eapply (incrementPC_success_updatePC _ σm) in H'regs'
+      as (p' & g' & b' & e' & a'' & a_pc' & HPC'' & HuPC & ->).
+    eapply updatePC_success_incl with (m':=σm) in HuPC. 2: by eapply insert_mono; eauto.
+    simplify_pair_eq.
+    iMod ((regspec_heap_update_inSepM _ _ _ dst (match wsrc with inl _ => inl 0%Z | inr _ => inl 1%Z end)) with "Hown Hmap") as "[Hown Hmap]"; eauto.
+    iMod ((regspec_heap_update_inSepM _ _ _ PC (inr (p', g', b', a''))) with "Hown Hmap") as "[Hown Hmap]"; eauto.
+    iMod (exprspec_mapsto_update _ _ (fill K (Instr NextI)) with "Hown Hj") as "[Hown Hj]".
+    iExists NextIV,_. iFrame.
+    iMod ("Hclose" with "[Hown]") as "_".
+    { iNext. iExists _,_;iFrame. iPureIntro. eapply rtc_r;eauto.
+      prim_step_from_exec.
+    }
+    iModIntro. iPureIntro. econstructor; eauto.
+    destruct wsrc; simpl in *; auto.
+  Qed.
+
+End cap_lang_spec_rules.
