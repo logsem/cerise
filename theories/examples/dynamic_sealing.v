8ac078085282727b1317e6369d2513ea2bc188fd
Alix Trieu <alix.trieu@cs.au.dk>
2021-02-03 16:40:29 +0100

    Write the code for dynamic sealing

--- theories/examples/dynamic_sealing.v
+++ theories/examples/dynamic_sealing.v
@@ -3,7 +3,8 @@ From iris.proofmode Require Import tactics.
 Require Import Eqdep_dec List.
 From cap_machine Require Import macros_helpers addr_reg_sample macros.
 From cap_machine Require Import rules logrel contiguous.
-From cap_machine Require Import monotone list call malloc.
+From cap_machine Require Import monotone list malloc.
+From cap_machine Require Import solve_pure proofmode map_simpl.
 
 Section sealing.
 
@@ -15,33 +16,67 @@ Section sealing.
   (* Assume r_t1 contains a capability representing a sealed value.
      Get its b field, find the corresponding value in the linked list contained in r_env.
      Result is returned in r_t1; clear r_env for security and return. *)
-  (* Arguments: r_t1 rret
+  (* Arguments: r_t1
+     Return point: r_t0
      Uses: r_env *)
-  Definition unseal_instr rret :=
-    [ getb r_t1 r_t1 ]
-    ++ findb_instr ++
-    [ move_z r_env 0
-    ; jmp rret
-    ].
+  Definition unseal_instrs :=
+    encodeInstrsW [ GetB r_t1 r_t1 ]
+                  ++ findb_instr ++
+    encodeInstrsW [ Mov r_env (inl 0%Z)
+                  ; Jmp r_t0
+                  ].
+
+  Definition unseal_instrs_length := Eval cbn in length (unseal_instrs).
 
   (* Assume r_t1 contains the word to seal.
      Append it to the linked list contained in r_env.
      Restrict the new capability to O permission.
      Subseg it, clear r_env and return *)
-  (* Arguments: r_t1 rret
-     Uses: r_env r_t0 *)
-  Definition seal_instr f_m rret :=
+  (* Arguments: r_t1
+     Return point: r_t0
+     Uses: r_env r_t2 *)
+  Definition seal_instrs f_m :=
     appendb_instr f_m ++
-    [ restrict_z r_t1 (encodePerm O)
-    ; getb r_t0 r_t1
-    ; subseg_r_r r_t1 r_t0 r_t0
-    ; move_z r_env 0
-    ; jmp rret
-    ].
+    encodeInstrsW [ Restrict r_t1 (inl (encodePerm O))
+                  ; GetB r_t2 r_t1
+                  ; Subseg r_t1 r_t2 r_t2
+                  ; Mov r_env (inl 0%Z)
+                  ; Jmp r_t0
+                  ].
 
-  (* Create two closures for sealing and unsealing *)
-  (* TODO *)
-  Definition make_seal f_m :=
-    malloc_instrs f_m (42).
+  Definition seal_instrs_length := Eval cbn in length (seal_instrs 0%Z).
+
+  (* Create two closures for sealing and unsealing assuming that their code is preceding this one *)
+  (* This works by:
+     1. Malloc an empty linked list, thus obtaining some capability c.
+     2. Create a closure with access to c, and containing the code unseal_instr.
+     3. Create a closure with access to c, and containing the code seal_instr.
+     4. Return closure for unsealing in r_t1 and sealing in r_t2. *)
+  (* Arguments: None
+     Return point: rt_0
+     Uses: r_t1 r_t2 r_t8 r_t9 r_t10 *)
+  Definition make_seal_preamble f_m :=
+    encodeInstrsW [ Mov r_t8 PC (* Copy PC into r_t8 *)
+                  ; Lea r_t8 (- (Z.of_nat seal_instrs_length))%Z (* Capability pointing to code for seal_instrs *)
+                  ]
+    ++ malloc_instrs f_m 1 ++ (* Malloc an empty list *)
+    encodeInstrsW [ Store r_t1 (inl 0%Z) (* Clear it *)
+                  ; Mov r_t9 r_t1 (* Keep a copy of the capability to the list *)
+                  ; Mov r_t2 r_t1 (* Prepare for creating closure, r_t2 must contain environment *)
+                  ; Mov r_t1 r_t8 (* Prepare for creating closure, r_t1 must contain code *)
+                  ]
+    ++ crtcls_instrs f_m ++ (* Create the closure for seal, result in r_t1 *)
+    encodeInstrsW [ Mov r_t10 r_t1 (* Closure for seal now in r_t10 *)
+                  ; Lea r_t8 (- (Z.of_nat unseal_instrs_length))%Z (* Capability pointing to code for seal_instrs *)
+                  ; Mov r_t1 r_t8 (* Prepare for creating closure for unseal, code *)
+                  ; Mov r_t1 r_t9 (* Prepare for creating closure for unseal, environment *)
+                  ]
+    ++ crtcls_instrs f_m ++ (* Create the closure for unseal, result in r_t1 *)
+    encodeInstrsW [ Mov r_t2 r_t10
+                  ; Mov r_t8 (inl 0%Z) (* Clearing registers *)
+                  ; Mov r_t9 (inl 0%Z)
+                  ; Mov r_t10 (inl 0%Z)
+                  ; Jmp r_t0 (* Return *)
+                  ].
 
 End sealing.
