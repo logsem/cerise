dbb2ffd6f1761a3e50745514a8ef46605e493aa3
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2021-03-10 10:26:28 +0100

    Fix [focus_block]: avoid unfolding definitions when looking for the block

---
+++ tests/proofmode_focus_block.v
@@ -0,0 +1,21 @@
+From iris.proofmode Require Import tactics.
+From cap_machine Require Import proofmode.
+
+Definition l1 `{MP: MachineParameters} := encodeInstrsW [ Mov r_t1 1; Mov r_t1 0 ].
+Definition i1 `{MP: MachineParameters} := encodeInstrsW [ Mov r_t2 1; Mov r_t2 0 ].
+Definition i2 `{MP: MachineParameters} := encodeInstrsW [ Mov r_t3 1; Mov r_t3 0 ].
+Definition l2 `{MP: MachineParameters} := i1 ++ i2.
+
+Lemma foo {Σ:gFunctors} {memg:memG Σ} {regg:regG Σ} `{MP: MachineParameters} p b e a :
+  {{{ ⌜ExecPCPerm p ∧ SubBounds b e a (a ^+ length (l1 ++ l2))%a⌝
+      ∗ codefrag a (l1 ++ l2)
+      ∗ PC ↦ᵣ inr (p,b,e,(a ^+ length l1)%a) }}}
+    Seq (Instr Executable)
+  {{{ RET HaltedV; True }}}.
+Proof.
+  iIntros (Φ) "(%He & Hcode & HPC) HΦ".
+  destruct He as [He Hs].
+  focus_block 1 "Hcode" as a_mid Ha_mid "Hblock" "Hcont".
+  (* check that "Hblock" is [codefrag a_mid l2] *)
+  iAssert (codefrag a_mid l2) with "Hblock" as "Hblock".
+Abort.
