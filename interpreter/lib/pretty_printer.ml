bc8050b0bae1c87949d52ef4d3682815d89c97d7
Andreas Stenbæk Larsen <jamanasl28@gmail.com>
2021-09-24 14:51:57 +0200

    Did some refactoring of printing parsed structures

---
+++ interpreter/lib/pretty_printer.ml
@@ -0,0 +1,50 @@
+open Ast
+
+let (^-) s1 s2 = s1 ^ " " ^ s2
+
+let string_of_regname (r: regname) : string =
+  match r with
+  | PC -> "pc"
+  | Reg i -> "r" ^ (string_of_int i)
+
+let string_of_perm (p: perm): string =
+  match p with
+  | O -> "O"
+  | E -> "E"
+  | RO -> "RO"
+  | RX -> "RX"
+  | RW -> "RW"
+  | RWX -> "RWX"
+
+let string_of_reg_or_const (c: reg_or_const) : string =
+  match c with
+  | Register r -> string_of_regname r
+  | Const c -> string_of_int c
+  | Perm p -> string_of_perm p
+
+let string_of_statement (s: statement): string =
+  let string_of_rr r1 r2 =
+    string_of_regname r1 ^- string_of_regname r2
+  and string_of_rc r c =
+    string_of_regname r ^- string_of_reg_or_const c
+  and string_of_rcc r c1 c2  =
+    string_of_regname r ^- string_of_reg_or_const c1 ^- string_of_reg_or_const c2
+  in match s with
+  | Jmp r -> "jmp" ^- string_of_regname r
+  | Jnz (r1, r2) -> "jnz" ^- string_of_rr r1 r2
+  | Move (r, c) -> "mov" ^- string_of_rc r c
+  | Load (r1, r2) -> "load" ^- string_of_rr r1 r2
+  | Store (r, c) -> "store" ^- string_of_rc r c
+  | Add (r, c1, c2) -> "add" ^- string_of_rcc r c1 c2
+  | Sub (r, c1, c2) -> "sub" ^- string_of_rcc r c1 c2
+  | Lt (r, c1, c2) -> "lt" ^- string_of_rcc r c1 c2
+  | Lea (r, c) -> "lea" ^- string_of_rc r c
+  | Restrict (r, c) -> "restrict" ^- string_of_rc r c
+  | SubSeg (r, c1, c2) -> "subseg" ^- string_of_rcc r c1 c2
+  | IsPtr (r1, r2) -> "isptr" ^- string_of_rr r1 r2
+  | GetP (r1, r2) -> "getp" ^- string_of_rr r1 r2
+  | GetB (r1, r2) -> "getb" ^- string_of_rr r1 r2
+  | GetE (r1, r2) -> "gete" ^- string_of_rr r1 r2
+  | GetA (r1, r2) -> "geta" ^- string_of_rr r1 r2
+  | Fail -> "fail"
+  | Halt -> "halt"
--- interpreter/lib/program.ml
+++ interpreter/lib/program.ml
@@ -1,48 +1,5 @@
-open Ast
-
-let print_register (r: regname) : unit =
-  match r with
-  | PC -> print_string "pc "
-  | Reg i -> print_string "r"; print_int i; print_char ' '
-
-let print_const (c: reg_or_const) : unit =
-  match c with
-  | Register r -> print_register r
-  | Const c -> print_int c; print_char ' '
-  | Perm p -> print_string (begin
-      match p with
-      | O -> "O "
-      | E -> "E "
-      | RO -> "RO "
-      | RX -> "RX "
-      | RW -> "RW "
-      | RWX -> "RWX "
-    end)
-
-let printer (s: statement): unit =
-  (match s with
-   | Jmp r -> print_string "jmp "; print_register r
-   | Jnz (r1, r2) -> print_string "jnz "; print_register r1; print_register r2
-   | Move (r, c) -> print_string "mov "; print_register r; print_const c
-   | Load (r1, r2) -> print_string "load "; print_register r1; print_register r2
-   | Store (r, c) -> print_string "store "; print_register r; print_const c
-   | Add (r, c1, c2) -> print_string "add "; print_register r; print_const c1; print_const c2
-   | Sub (r, c1, c2) -> print_string "sub "; print_register r; print_const c1; print_const c2
-   | Lt (r, c1, c2) -> print_string "lt "; print_register r; print_const c1; print_const c2
-   | Lea (r, c) -> print_string "lea "; print_register r; print_const c
-   | Restrict (r, c) -> print_string "restrict "; print_register r; print_const c
-   | SubSeg (r, c1, c2) -> print_string "subseg "; print_register r; print_const c1; print_const c2
-   | IsPtr (r1, r2) -> print_string "isptr "; print_register r1; print_register r2
-   | GetP (r1, r2) -> print_string "getp "; print_register r1; print_register r2
-   | GetB (r1, r2) -> print_string "getb "; print_register r1; print_register r2
-   | GetE (r1, r2) -> print_string "gete "; print_register r1; print_register r2
-   | GetA (r1, r2) -> print_string "geta "; print_register r1; print_register r2
-   | Fail -> print_string "fail "
-   | Halt -> print_string "halt "
-  ); print_newline()
-      
 let parse (_filename: string): unit (*(Ast.t, string) Result.t*) =
   let input = open_in _filename in
   let filebuf = Lexing.from_channel input in
   let parse_res = Parser.main Lexer.token filebuf in
-  close_in input; List.iter printer parse_res
+  close_in input; List.iter (fun s -> print_endline @@ Pretty_printer.string_of_statement s) parse_res
