84b7191e6c2a37079a05a82bda847a3e6cfbc9cb
Andreas Stenbæk Larsen <jamanasl28@gmail.com>
2021-10-19 00:27:37 +0200

    added execution for some more instructions

--- interpreter/lib/machine.ml
+++ interpreter/lib/machine.ml
@@ -20,7 +20,7 @@ type exec_state = Running | Halted | Failed
    but might not be as true to the model as doing: Cap of Z.t * Z.t * Z.t * Z.t *)
 type word = I of Z.t | Cap of perm * int * int * int 
 type reg_state = word RegMap.t
-type mem_state = Z.t MemMap.t
+type mem_state = word MemMap.t
 type exec_conf = { reg : reg_state; mem : mem_state } (* using a record to have notation similar to the paper *)
 type mchn = exec_state * exec_conf
 
@@ -32,17 +32,20 @@ let init_reg_state (addr_max : int) : reg_state =
   RegMap.of_seq seq
 
 let get_reg (r : regname) ({reg ; _} : exec_conf) : word = RegMap.find r reg
-
+let (@!) x y = get_reg x y
+  
 let upd_reg (r : regname) (w : word) ({reg ; mem} : exec_conf) : exec_conf =
   {reg = RegMap.add r w reg ; mem}
-    
+
+
 let init_mem_state (prog : t) : mem_state =
-  let enc_prog = List.to_seq @@ List.mapi (fun i x -> i, (Encode.encode_statement x)) prog in
+  let enc_prog = List.to_seq @@ List.mapi (fun i x -> i, I (Encode.encode_statement x)) prog in
   MemMap.add_seq enc_prog MemMap.empty
 
-let get_mem (addr : int) (conf : exec_conf) : Z.t option = MemMap.find_opt addr conf.mem
+let get_mem (addr : int) (conf : exec_conf) : word option = MemMap.find_opt addr conf.mem
+let (@?) x y = get_mem x y
 
-let upd_mem (addr : int) (i : Z.t) ({reg ; mem} : exec_conf) : exec_conf = {reg ; mem = MemMap.add addr i mem}
+let upd_mem (addr : int) (w : word) ({reg ; mem} : exec_conf) : exec_conf = {reg ; mem = MemMap.add addr w mem}
 
 let init_mchn (addr_max : int) (prog : t) : mchn =
   let regs = init_reg_state addr_max in
@@ -56,48 +59,102 @@ let get_word (conf : exec_conf) (roc : reg_or_const) : word =
   | CP (Perm p) -> I (Encode.encode_perm p) (* A permission is just an integer in the model *)
 
 let upd_pc (conf : exec_conf) : mchn =
-  match get_reg PC conf with
+  match PC @! conf with
   | Cap (p, b, e, a) -> (Running, upd_reg PC (Cap (p, b, e, a+1)) conf)
   | _ -> (Failed, conf)
+let (!>) conf = upd_pc conf
 
 let upd_pc_perm (w : word) =
   match w with
   | Cap (E, b, e, a) -> Cap (RX, b, e, a)
   | _ -> w
 
-let fetch_decode (conf : exec_conf) : statement =
+let fetch_decode (conf : exec_conf) : statement option =
   let addr =
-    match get_reg PC conf with
+    match PC @! conf with
     | Cap (_, _, _, a) -> a
-    | _ -> raise RuntimeException
+    | _ -> raise RuntimeException (* TODO: Fix this ugliness *)
   in
   match get_mem addr conf with
-  | Some enc -> Encode.decode_statement enc
-  | _ -> raise RuntimeException
+  | Some (I enc) -> Some (Encode.decode_statement enc)
+  | _ -> None
 
 let is_pc_valid (conf : exec_conf) : bool =
-  match get_reg PC conf with
+  match PC @! conf with
   | Cap ((RX|RWX), b, e, a) -> begin
       if b <= a && a < e
-      then Option.is_some @@ get_mem a conf
+      then Option.is_some @@ a @? conf
       else false
     end
   | _ -> false
 
+(* Might change this later since it is a bit too dependent
+   on the encode implementation *)
+let flowsto (p1 : perm) (p2 : perm) : bool =
+  let open Encode in
+  let enc1 = encode_perm p1 in
+  let enc2 = encode_perm p2 in
+  Z.(equal (enc1 lor enc2) enc2)
+
 let exec_single (conf : exec_conf) : mchn =
-  if is_pc_valid conf
-  then begin
-    let instr = fetch_decode conf in
-    match instr with
-    | Fail -> (Failed, conf)
-    | Halt -> (Halted, conf)
-    | Move (r, c) -> begin
-        let w = get_word conf c in
-        upd_pc @@ upd_reg r w conf
+  let fail_state = (Failed, conf) in
+  if is_pc_valid conf 
+  then match fetch_decode conf with
+    | None -> fail_state
+    | Some instr -> begin
+        match instr with
+        | Fail -> (Failed, conf)
+        | Halt -> (Halted, conf)
+        | Move (r, c) -> begin
+            let w = get_word conf c in
+            upd_pc @@ upd_reg r w conf
+          end
+        | Load (r1, r2) -> begin
+            match r2 @! conf with
+            | Cap ((RO|RX|RW|RWX), b, e, a) -> begin
+                match a @? conf with
+                | Some w when (b <= a && a < e) -> !> (upd_reg r1 w conf)
+                | _ -> fail_state
+              end
+            | _ -> fail_state
+          end
+        | Store (r, c) -> begin
+            match r @! conf with
+            | Cap ((RW|RWX), b, e, a) when (b <= a && a < e) -> begin
+                let w = get_word conf c in
+                !> (upd_mem a w conf)
+              end
+            | _ -> fail_state
+          end
+        | Jmp r -> begin
+            let new_pc = upd_pc_perm (r @! conf) in
+            (Running, upd_reg PC new_pc conf)
+          end
+        | Jnz (r1, r2) -> begin
+            match r2 @! conf with
+            | I i when Z.(equal i zero) -> !> conf
+            | _ -> begin
+                let new_pc = upd_pc_perm (r1 @! conf) in
+                (Running, upd_reg PC new_pc conf)
+              end
+          end
+        | Restrict (r, c) -> begin
+            match r @! conf with
+            | Cap (p, b, e, a) -> begin
+                match get_word conf c with
+                | I i -> begin
+                    let p' = Encode.decode_perm i in
+                    if flowsto p' p
+                    then !> (upd_reg PC (Cap (p', b, e, a)) conf)
+                    else fail_state
+                  end
+                | _ -> fail_state
+              end
+            | _ -> fail_state
+          end
+        | _ -> fail_state       
       end
-    | _ -> raise NotYetImplemented        
-  end  
-  else raise RuntimeException
+  else fail_state
 
 let rec run (st, conf : mchn) : mchn =
   match st with
