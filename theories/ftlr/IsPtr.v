375707fbb61422b3afcc7eed87186871c1f47d38
Thomas Van Strydonck <Thomas.VanStrydonck@cs.kuleuven.be>
2021-11-15 18:50:16 +0100

    IsPtr case

--- theories/ftlr/IsPtr.v
+++ theories/ftlr/IsPtr.v
@@ -6,7 +6,7 @@ From cap_machine.ftlr Require Import ftlr_base.
 From cap_machine.rules Require Import rules_IsPtr.
 
 Section fundamental.
-  Context {Σ:gFunctors} {memg:memG Σ} {regg:regG Σ}
+  Context {Σ:gFunctors} {memg:memG Σ} {regg:regG Σ} {sealsg: sealStoreG Σ}
           {nainv: logrel_na_invs Σ}
           `{MachineParameters}.
 
