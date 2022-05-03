5cdb9f9081ee97183d4195c3f4aec215ba70665f
Bastien Rousseau <bastien.rousseau@ens-rennes.fr>
2022-05-03 16:20:13 +0200

    CAS weakest precondition detailed rules

--- theories/rules/rules_CAS.v
+++ theories/rules/rules_CAS.v
@@ -45,21 +45,21 @@ Section cap_lang_rules.
       mem = mem' ->
       Cas_failure_spec i regs loc cond newvalue mem  ->
       Cas_spec i regs regs' loc cond newvalue mem mem' (i, FailedV)
-  | Cas_spec_success_eq p b e a (c oldv newv : Word) :
+  | Cas_spec_success_eq p b e a (oldv newv : Word) :
       reg_allows_store i regs loc p b e a  →
       mem !! a = Some oldv →
-      (* regs !! (i, cond) = Some oldv -> *)
-      (* regs !! (i, newvalue) = Some newv -> *)
-      oldv = c ->
+      regs !! (i, cond) = Some oldv ->
+      regs !! (i, newvalue) = Some newv ->
       mem' = (<[a := newv]> mem) →
       incrementPC (<[ (i, cond) := oldv ]> regs) i = Some regs' ->
       Cas_spec i regs regs' loc cond newvalue mem mem' (i, NextIV)
   | Cas_spec_success_neq p b e a (c oldv newv : Word) :
       reg_allows_store i regs loc p b e a  →
-      mem !! a = Some oldv →
-      (* regs !! (i, cond) = Some oldv -> *)
-      (* regs !! (i, newvalue) = Some newv -> *)
       oldv ≠ c ->
+      mem !! a = Some oldv →
+      mem = mem' ->
+      regs !! (i, cond) = Some c ->
+      regs !! (i, newvalue) = Some newv ->
       incrementPC (<[ (i, cond) := oldv ]> regs) i = Some regs' ->
       Cas_spec i regs regs' loc cond newvalue mem mem' (i, NextIV)
 .
@@ -94,7 +94,7 @@ Section cap_lang_rules.
   Qed.
 
 
-   Lemma wp_Cas Ep i
+  Lemma wp_Cas Ep i
      pc_p pc_b pc_e pc_a
      (loc cond newvalue : RegName)
      w mem regs :
@@ -294,6 +294,211 @@ Section cap_lang_rules.
 
 
 
+  Lemma wp_cas_success_eq_PC
+    E i r2 r3
+    pc_b pc_e pc_a pc_a'
+    instr newv v
+    :
+    decodeInstrW instr = CAS PC r2 r3 →
+    isCorrectPC (WCap RWX pc_b pc_e pc_a) →
+    (pc_a + 1)%a = Some pc_a' →
+
+
+    {{{ ▷ (i, PC) ↦ᵣ WCap RWX pc_b pc_e pc_a
+        ∗ ▷ pc_a ↦ₐ instr
+        ∗ ▷ (i, r2) ↦ᵣ instr
+        ∗ ▷ (i, r3) ↦ᵣ newv }}}
+      (i, Instr Executable) @ E
+      {{{ RET (i, NextIV);
+          (i, PC) ↦ᵣ WCap RWX pc_b pc_e pc_a'
+          ∗ pc_a ↦ₐ newv
+          ∗ (i, r2) ↦ᵣ instr
+          ∗ (i, r3) ↦ᵣ newv }}}.
+  Proof.
+    intros. iIntros "(>HPC & >Hpc_a & Hr2 & Hr3) Hφ".
+    iDestruct (map_of_regs_3 with "HPC Hr2 Hr3") as ">[Hmap Hregs]".
+    iDestruct "Hregs" as "[% [% %]]".
+    iDestruct (memMap_resource_1 with "Hpc_a") as "Hmem".
+
+    iApply (wp_Cas with "[$Hmap $Hmem]"); eauto
+    ; simplify_map_eq by simplify_pair_eq; eauto.
+    { rewrite /regs_of_core ; set_solver. }
+    { eapply mem_eq_implies_allow_store_map; eauto.
+      all : by simplify_map_eq by simplify_pair_eq. }
+    iModIntro.
+    iIntros (regs' mem' retv) "[#Hspec [Hmem Hregs]]".
+    iDestruct "Hspec" as %Hspec.
+
+    inversion Hspec ; subst.
+    - (* Failure *)
+      destruct H10; try incrementPC_inv; simplify_map_eq by simplify_pair_eq
+      ; eauto.
+      inversion H3 ; subst.
+      destruct o. destruct H13 ; subst ; cbn in *.
+      all: try congruence.
+      rewrite /withinBounds in H8; solve_addr.
+    - (* Success with equality *)
+      iApply "Hφ".
+      (* destruct H4 as [Hrr2 _]. *)
+      incrementPC_inv.
+      simplify_map_eq by simplify_pair_eq.
+      destruct ( decide (x2 = a) ) ; subst ; simplify_map_eq.
+      rewrite (insert_commute _ (i, PC) (i, r2)) ; simplify_pair_eq.
+      rewrite insert_insert insert_insert.
+      rewrite (insert_commute _ (i, r2) (i, PC)) ; simplify_pair_eq.
+      rewrite insert_insert.
+      iFrame.
+      iDestruct (regs_of_map_3 with "[$Hregs]") as "(HPC & Hr2 & Hr3)"
+      ; eauto.
+      rewrite memMap_resource_1.
+      iFrame.
+    - (* Success with non-equality *)
+      incrementPC_inv.
+      simplify_map_eq by simplify_pair_eq.
+      destruct ( decide (x2 = a) ) ; subst ; simplify_map_eq.
+  Qed.
+
+
+  Lemma wp_cas_success_neq_PC
+    E i r2 r3
+    pc_b pc_e pc_a pc_a'
+    instr cond newv v
+    :
+    decodeInstrW instr = CAS PC r2 r3 →
+    isCorrectPC (WCap RWX pc_b pc_e pc_a) →
+    (pc_a + 1)%a = Some pc_a' →
+    cond ≠ instr ->
+
+    {{{ ▷ (i, PC) ↦ᵣ WCap RWX pc_b pc_e pc_a
+        ∗ ▷ pc_a ↦ₐ instr
+        ∗ ▷ (i, r2) ↦ᵣ cond
+        ∗ ▷ (i, r3) ↦ᵣ newv }}}
+      (i, Instr Executable) @ E
+      {{{ RET (i, NextIV);
+          (i, PC) ↦ᵣ WCap RWX pc_b pc_e pc_a'
+          ∗ pc_a ↦ₐ instr
+          ∗ (i, r2) ↦ᵣ instr
+          ∗ (i, r3) ↦ᵣ newv }}}.
+  Proof.
+    intros. iIntros "(>HPC & >Hpc_a & Hr2 & Hr3) Hφ".
+    iDestruct (map_of_regs_3 with "HPC Hr2 Hr3") as ">[Hmap Hregs]".
+    iDestruct "Hregs" as "[% [% %]]".
+    iDestruct (memMap_resource_1 with "Hpc_a") as "Hmem".
+
+    iApply (wp_Cas with "[$Hmap $Hmem]"); eauto
+    ; simplify_map_eq by simplify_pair_eq; eauto.
+    { rewrite /regs_of_core ; set_solver. }
+    { eapply mem_eq_implies_allow_store_map; eauto.
+      all : by simplify_map_eq by simplify_pair_eq. }
+    iModIntro.
+    iIntros (regs' mem' retv) "[#Hspec [Hmem Hregs]]".
+    iDestruct "Hspec" as %Hspec.
+
+    inversion Hspec ; subst.
+    - (* Failure *)
+      destruct H11; try incrementPC_inv; simplify_map_eq by simplify_pair_eq
+      ; eauto.
+      inversion H3 ; subst.
+      destruct o. destruct H11 ; subst ; cbn in *.
+      all: try congruence.
+      rewrite /withinBounds in H9; solve_addr.
+    - (* Success with equality *)
+      incrementPC_inv.
+      simplify_map_eq by simplify_pair_eq.
+      destruct ( decide (x2 = a) ) ; subst ; simplify_map_eq.
+    - (* Success with non-equality *)
+      iApply "Hφ".
+      incrementPC_inv.
+      simplify_map_eq by simplify_pair_eq.
+      destruct ( decide (x2 = a) ) ; subst ; simplify_map_eq.
+      rewrite (insert_commute _ (i, PC) (i, r2)) ; simplify_pair_eq.
+      rewrite insert_insert.
+      rewrite (insert_commute _ (i, r2) (i, PC)) ; simplify_pair_eq.
+      rewrite insert_insert.
+      inversion H9 as [? [ contra _]].
+      match goal with
+      | h: context[ ?m !! ?r = Some _ ] |- _ =>
+          let Heq := fresh "Heq" in
+          assert (Heq : m !! r = Some ( WCap RWX x0 x1 a ))
+            by (simplify_map_eq by simplify_pair_eq ; reflexivity)
+          ; rewrite h in Heq ; simplify_eq
+      end.
+      iDestruct (regs_of_map_3 with "[$Hregs]") as "(HPC & Hr2 & Hr3)"
+      ; eauto.
+      rewrite memMap_resource_1.
+      iFrame.
+  Qed.
+
+  Lemma wp_cas_fail_PC
+    E i r2 r3
+    pc_b pc_e pc_a pc_a'
+    instr cond newv v
+    :
+    decodeInstrW instr = CAS PC r2 r3 →
+    isCorrectPC (WCap RX pc_b pc_e pc_a) →
+    (pc_a + 1)%a = Some pc_a' →
+
+
+    {{{ ▷ (i, PC) ↦ᵣ WCap RX pc_b pc_e pc_a
+        ∗ ▷ pc_a ↦ₐ instr
+        ∗ ▷ (i, r2) ↦ᵣ cond
+        ∗ ▷ (i, r3) ↦ᵣ newv }}}
+      (i, Instr Executable) @ E
+      {{{ RET (i, FailedV);
+          (i, PC) ↦ᵣ WCap RX pc_b pc_e pc_a
+          ∗ pc_a ↦ₐ instr
+          ∗ (i, r2) ↦ᵣ cond
+          ∗ (i, r3) ↦ᵣ newv }}}.
+  Proof.
+    intros. iIntros "(>HPC & >Hpc_a & Hr2 & Hr3) Hφ".
+    iDestruct (map_of_regs_3 with "HPC Hr2 Hr3") as ">[Hmap Hregs]".
+    iDestruct "Hregs" as "[% [% %]]".
+    iDestruct (memMap_resource_1 with "Hpc_a") as "Hmem".
+
+    iApply (wp_Cas with "[$Hmap $Hmem]"); eauto
+    ; simplify_map_eq by simplify_pair_eq; eauto.
+    { rewrite /regs_of_core ; set_solver. }
+    { eapply mem_eq_implies_allow_store_map; eauto.
+      all : by simplify_map_eq by simplify_pair_eq. }
+    iModIntro.
+    iIntros (regs' mem' retv) "[#Hspec [Hmem Hregs]]".
+    iDestruct "Hspec" as %Hspec.
+
+    inversion Hspec ; subst.
+    - (* Failure *)
+      destruct H10; try incrementPC_inv; simplify_map_eq by simplify_pair_eq
+      ; eauto ; try congruence.
+      destruct o.
+      2: { inversion H3 ; subst.
+           rewrite /withinBounds in H8 ; solve_addr. }
+      iApply "Hφ".
+      iDestruct (regs_of_map_3 with "[$Hregs]") as "(HPC & Hr2 & Hr3)"
+      ; eauto.
+      rewrite memMap_resource_1.
+      iFrame.
+    - (* Success with equality - contradiction *)
+      clear -H8.
+      inversion H8 as [? [ contra _]].
+      match goal with
+      | h: context[ ?x = Some _ ] |- _ =>
+          let Heq := fresh "Heq" in
+          assert (Heq : x = Some ( WCap RX pc_b pc_e pc_a ))
+            by (simplify_map_eq by simplify_pair_eq ; reflexivity)
+          ; rewrite h in Heq ; simplify_eq
+      end.
+    - (* Success with non-equality - contradiction *)
+      clear -H8.
+      inversion H8 as [? [ contra _]].
+      match goal with
+      | h: context[ ?x = Some _ ] |- _ =>
+          let Heq := fresh "Heq" in
+          assert (Heq : x = Some ( WCap RX pc_b pc_e pc_a ))
+            by (simplify_map_eq by simplify_pair_eq ; reflexivity)
+          ; rewrite h in Heq ; simplify_eq
+      end.
+  Qed.
+
+
   Lemma wp_cas_success_eq
     E i r1 r2 r3
     pc_p pc_b pc_e pc_a pc_a'
@@ -308,82 +513,183 @@ Section cap_lang_rules.
     {{{ ▷ (i, PC) ↦ᵣ WCap pc_p pc_b pc_e pc_a
         ∗ ▷ pc_a ↦ₐ instr
         ∗ ▷ (i, r1) ↦ᵣ (WCap p b e a)
-        ∗ (if (eqb_addr a pc_a) then emp else ▷ a ↦ₐ oldv)
+        ∗ ▷ a ↦ₐ oldv
         ∗ ▷ (i, r2) ↦ᵣ cond
         ∗ ▷ (i, r3) ↦ᵣ newv }}}
       (i, Instr Executable) @ E
       {{{ RET (i, NextIV);
           (i, PC) ↦ᵣ WCap pc_p pc_b pc_e pc_a'
-        ∗ pc_a ↦ₐ (if (a =? pc_a)%a then newv else instr)
+        ∗ pc_a ↦ₐ instr
         ∗ (i, r1) ↦ᵣ (WCap p b e a)
-        ∗ (if (eqb_addr a pc_a) then emp else ▷ a ↦ₐ newv)
+        ∗ ▷ a ↦ₐ newv
         ∗ (i, r2) ↦ᵣ oldv
         ∗ (i, r3) ↦ᵣ newv }}}.
-  (* TODO *)
    Proof.
-     intros. iIntros "(>HPC & >Hpc_a & Hr1 & Hloc & Hr2 & Hr3) Hφ".
-     iDestruct (map_of_regs_4 with "HPC Hr1 Hr2 Hr3") as ">[Hmap Hregs]".
+     intros. iIntros "(>HPC & >Hpc_a & >Hr1 & >Hloc & >Hr2 & >Hr3) Hφ".
+     iDestruct (map_of_regs_4 with "HPC Hr1 Hr2 Hr3") as "[Hmap Hregs]".
      iDestruct "Hregs" as "[% [% [% [% [% %]]]]]".
-     iDestruct (memMap_resource_2gen_clater pc_a a _ _ (λ a w, a ↦ₐ w)%I  with
-                 "Hpc_a Hloc")
-       as (mem) "[Hmem %]".
+    iDestruct (memMap_resource_2ne_apply with "Hpc_a Hloc") as "[Hmem %]"; auto.
 
     iApply (wp_Cas with "[$Hmap $Hmem]"); eauto
     ; simplify_map_eq by simplify_pair_eq; eauto.
     { rewrite /regs_of_core ; set_solver. }
-    { destruct (a =? pc_a)%a; by simplify_map_eq by simplify_pair_eq. }
-    { eapply mem_implies_allow_store_map; eauto.
+    { eapply mem_neq_implies_allow_store_map with (a := a); eauto.
       all : by simplify_map_eq by simplify_pair_eq. }
     iModIntro.
     iIntros (regs' mem' retv) "[#Hspec [Hmem Hregs]]".
     iDestruct "Hspec" as %Hspec.
-    inversion Hspec ; subst.
-    - (* Failure *)
-      destruct H16; try incrementPC_inv; simplify_map_eq by simplify_pair_eq; eauto.
+    inversion Hspec as [ _ _ Hfail | | ] ; subst.
+    - (* Failure - contradiction *)
+      destruct Hfail; try incrementPC_inv; simplify_map_eq by simplify_pair_eq; eauto.
        destruct o. all: congruence.
     - (* Success with equality *)
       iApply "Hφ".
-      destruct H4 as [Hrr2 _]. simplify_map_eq by simplify_pair_eq.
-     (*  destruct ( decide (a0 = pc_a)) *)
-     (*  ; simplify_eq *)
-     (*  ; subst. *)
-     (*  + (* a0 = pc_a *) *)
-     (*  rewrite Z.eqb_refl in H14 ; rewrite Z.eqb_refl. *)
-     (*  subst. *)
-     (*  rewrite memMap_resource_1. *)
-     (*   incrementPC_inv. *)
-     (*   simplify_map_eq by simplify_pair_eq. *)
-     (*   rewrite (insert_commute _ (i, PC) (i, r2)) ; simplify_pair_eq. *)
-     (*   rewrite insert_insert insert_insert. *)
-     (*   rewrite (insert_commute _ (i, r2) (i, PC)) ; simplify_pair_eq. *)
-     (*   rewrite (insert_commute _ (i, r2) (i, r1)) ; simplify_pair_eq. *)
-     (*   rewrite insert_insert. *)
-     (*   iDestruct (regs_of_map_4 with "[$Hregs]") as "(HPC & Hr1 & Hr2 & Hr3)" *)
-     (*   ; eauto. iFrame. *)
+      destruct H4 as [Hrr2 _].
+      incrementPC_inv.
+      simplify_map_eq by simplify_pair_eq.
+      rewrite (insert_commute _ (i, PC) (i, r2)) ; simplify_pair_eq.
+      rewrite insert_insert.
+      rewrite (insert_commute _ (i, r2) (i, PC)) ; simplify_pair_eq.
+      rewrite (insert_commute _ (i, r2) (i, r1)) ; simplify_pair_eq.
+      rewrite insert_insert.
+      rewrite (insert_commute _ x2 a0) ; simplify_pair_eq ; last eassumption.
+      rewrite insert_insert.
+      iDestruct (regs_of_map_4 with "[$Hregs]") as "(HPC & Hr1 & Hr2 & Hr3)"
+      ; eauto.
+      rewrite memMap_resource_2ne ;
+        last (apply not_eq_sym ; eassumption).
+      iDestruct "Hmem" as "[? ?]".
+      iFrame.
+    - (* Success with non-equality - contradiction *)
+      incrementPC_inv.
+      simplify_map_eq by simplify_pair_eq.
+      rewrite (insert_commute _ (i, PC) (i, r2)) ; simplify_pair_eq.
+      rewrite insert_insert.
+      rewrite (insert_commute _ (i, r2) (i, PC)) ; simplify_pair_eq.
+      rewrite (insert_commute _ (i, r2) (i, r1)) ; simplify_pair_eq.
+      rewrite insert_insert.
 
-     (*   destruct ( (a0 =? x2)%Z ) eqn:Heq_addr *)
-     (*   ; [ rewrite -> Z.eqb_eq in Heq_addr | rewrite -> Z.eqb_neq in Heq_addr ] *)
-     (*   ; subst *)
-     (*   ; simplify_eq. *)
-     (*   apply lookup_insert_Some in H15 ; destruct H15 as [ [? ?]|?]. *)
-     (*   simplify_eq. *)
-     (*   iFrame. *)
-     (*   rewrite lookup_insert in H15. *)
+      iDestruct (regs_of_map_4 with "[$Hregs]") as "(HPC & Hr1 & Hr2 & Hr3)"
+      ; eauto.
+      rewrite memMap_resource_2ne ; last eassumption.
+      iDestruct "Hmem" as "[Hx2 Ha]".
+      rename x into pc_p
+      ; rename x0 into pc_b
+      ; rename x1 into pc_e
+      ; rename x2 into pc_a.
+      inversion H4.
+      match goal with
+      | h: context[ ?x = Some _ ] |- _ =>
+          match x with
+          | context[ <[(i, PC):=WCap pc_p pc_b pc_e pc_a]> _ ] =>
+              let Heq := fresh "Heq" in
+              assert (Heq : x = Some ( WCap p b e a ))
+                by (simplify_map_eq by simplify_pair_eq ; reflexivity)
+              ; rewrite Heq in h ; simplify_eq ; clear Heq
+          end
+      end.
+      simplify_map_eq by simplify_pair_eq.
+   Qed.
 
+  Lemma wp_cas_success_neq
+    E i r1 r2 r3
+    pc_p pc_b pc_e pc_a pc_a'
+    instr p b e a oldv cond newv
+    :
+   decodeInstrW instr = CAS r1 r2 r3 →
+   isCorrectPC (WCap pc_p pc_b pc_e pc_a) →
+   oldv ≠ cond ->
+   (pc_a + 1)%a = Some pc_a' →
+   writeAllowed p = true → withinBounds b e a = true →
 
-     (* } iFrame. *)
+    {{{ ▷ (i, PC) ↦ᵣ WCap pc_p pc_b pc_e pc_a
+        ∗ ▷ pc_a ↦ₐ instr
+        ∗ ▷ (i, r1) ↦ᵣ (WCap p b e a)
+        ∗ ▷ a ↦ₐ oldv
+        ∗ ▷ (i, r2) ↦ᵣ cond
+        ∗ ▷ (i, r3) ↦ᵣ newv }}}
+      (i, Instr Executable) @ E
+      {{{ RET (i, NextIV);
+          (i, PC) ↦ᵣ WCap pc_p pc_b pc_e pc_a'
+        ∗ pc_a ↦ₐ instr
+        ∗ (i, r1) ↦ᵣ (WCap p b e a)
+        ∗ ▷ a ↦ₐ oldv
+        ∗ (i, r2) ↦ᵣ oldv
+        ∗ (i, r3) ↦ᵣ newv }}}.
+   Proof.
+     intros. iIntros "(>HPC & >Hpc_a & >Hr1 & >Hloc & >Hr2 & >Hr3) Hφ".
+     iDestruct (map_of_regs_4 with "HPC Hr1 Hr2 Hr3") as "[Hmap Hregs]".
+     iDestruct "Hregs" as "[% [% [% [% [% %]]]]]".
+    iDestruct (memMap_resource_2ne_apply with "Hpc_a Hloc") as "[Hmem %]"; auto.
 
-     (*  iFrame. *)
+    iApply (wp_Cas with "[$Hmap $Hmem]"); eauto
+    ; simplify_map_eq by simplify_pair_eq; eauto.
+    { rewrite /regs_of_core ; set_solver. }
+    { eapply mem_neq_implies_allow_store_map with (a := a); eauto.
+      all : by simplify_map_eq by simplify_pair_eq. }
+    iModIntro.
+    iIntros (regs' mem' retv) "[#Hspec [Hmem Hregs]]".
+    iDestruct "Hspec" as %Hspec.
+    inversion Hspec as [ _ _ Hfail | | ] ; subst.
+    - (* Failure - contradiction *)
+      destruct Hfail; try incrementPC_inv; simplify_map_eq by simplify_pair_eq; eauto.
+       destruct o. all: congruence.
+    - (* Success with equality *)
+      incrementPC_inv.
+      simplify_map_eq by simplify_pair_eq.
+      rename x into pc_p
+      ; rename x0 into pc_b
+      ; rename x1 into pc_e
+      ; rename x2 into pc_a.
+      inversion H15.
+      match goal with
+      | h: context[ ?x = Some _ ] |- _ =>
+          match x with
+          | context[ <[(i, PC):=WCap pc_p pc_b pc_e pc_a]> _ ] =>
+              let Heq := fresh "Heq" in
+              assert (Heq : x = Some ( WCap p b e a ))
+                by (simplify_map_eq by simplify_pair_eq ; reflexivity)
+              ; rewrite Heq in h ; simplify_eq ; clear Heq
+          end
+      end.
 
-     (*  (* Failure *) *)
+      clear -H4 H14 H16.
+      simplify_map_eq by simplify_pair_eq.
+    - (* Success with non-equality - contradiction *)
+      incrementPC_inv.
+      simplify_map_eq by simplify_pair_eq.
+      rename x into pc_p
+      ; rename x0 into pc_b
+      ; rename x1 into pc_e
+      ; rename x2 into pc_a.
+      inversion H15.
+      match goal with
+      | h: context[ ?x = Some _ ] |- _ =>
+          match x with
+          | context[ <[(i, PC):=WCap pc_p pc_b pc_e pc_a]> _ ] =>
+              let Heq := fresh "Heq" in
+              assert (Heq : x = Some ( WCap p b e a ))
+                by (simplify_map_eq by simplify_pair_eq ; reflexivity)
+              ; rewrite Heq in h ; simplify_eq ; clear Heq
+          end
+      end.
 
+      rewrite (insert_commute _ (i, PC) (i, r2)) ; simplify_pair_eq.
+      rewrite insert_insert.
+      rewrite (insert_commute _ (i, r2) (i, PC)) ; simplify_pair_eq.
+      rewrite (insert_commute _ (i, r2) (i, r1)) ; simplify_pair_eq.
+      rewrite insert_insert.
 
+      iApply "Hφ".
+      iDestruct (regs_of_map_4 with "[$Hregs]") as "(HPC & Hr1 & Hr2 & Hr3)"
+      ; eauto.
+      rewrite memMap_resource_2ne ; last eassumption.
+      iDestruct "Hmem" as "[? ?]".
+      replace oldv with oldv0.
+      iFrame.
+      clear - H14 H17.
+      by simplify_map_eq.
+   Qed.
 
-
-     (* iApply (wp_store_success_reg' with "[$HPC $Hpc_a $Hdst]"); eauto. *)
-     (* { rewrite Z.eqb_refl. eauto. } *)
-     (* iNext. iIntros "(? & ? & ? & ?)". rewrite Z.eqb_refl. *)
-     (* iApply "Hφ". iFrame. Unshelve. eauto. *)
-   Admitted.
 
 End cap_lang_rules.
