1140abd4b7872cc9b2665cc30f155ec8d8c7675d
Dorian Lesbre <dorian.lesbre@gmail.com>
2023-01-09 10:10:19 +0100

    Better definition for contextual refinement

--- theories/contextual_refinement.v
+++ theories/contextual_refinement.v
@@ -7,25 +7,6 @@ From cap_machine Require Import machine_parameters cap_lang linking machine_run.
 Section contextual_refinement.
   Context `{MachineParameters}.
 
-  Lemma machine_run_ends_in_halted_or_error:
-    ∀ n init c, machine_run n init = Some c -> c = Halted \/ c = Failed.
-  Proof.
-    intros n.
-    induction n; intros init c mr.
-    - discriminate mr.
-    - simpl in mr.
-      destruct init as [[ | | | ] [r m]].
-      destruct (r !! PC).
-      destruct (isCorrectPCb w).
-      destruct (m !! match w with
-      | WInt _ => finz.largest 0%a
-      | WCap _ _ _ a => a
-      end). apply IHn in mr. apply mr.
-      1,2,3,5: right; symmetry; apply Some_inj in mr; apply mr.
-      left; symmetry. apply Some_inj in mr; apply mr.
-      apply IHn in mr. apply mr.
-  Qed.
-
   Variable b_stk: Addr.
   Variable e_stk: Addr.
 
@@ -74,14 +55,16 @@ Section contextual_refinement.
 
   #[local] Notation wf_pre_comp := (well_formed_pre_comp e_stk Symbols _ _ word_restrictions).
 
+  #[local] Notation make_link_main_lib := (make_link_main_lib Symbols _ _).
+
   (** Register that holds the stack capability. Any != PC will do
       since we quantify over all programs *)
   Definition r_stk := r_t1.
 
   Section ctxt_ref_def.
-    Variable implementation specification: m_pre_component.
-    Variable wf_impl : wf_pre_comp implementation.
-    Variable wf_spec : wf_pre_comp specification.
+
+    (** Component with proof of their well-formedness *)
+    Definition component_wf := { x: m_pre_component | wf_pre_comp x }.
 
     (** Create a initial machine state from a component:
         - registers are all 0, except PC which is main, and r_stk which points to the stack
@@ -94,39 +77,45 @@ Section contextual_refinement.
         (<[PC := c_main]> (gset_to_gmap (WInt 0) (list_to_set all_registers))), ms)
       end.
 
-    Definition contextual_refinement :=
-      forall (context linked_impl linked_spec: m_component),
-        is_context context (Lib implementation) linked_impl ->
-        is_context context (Lib specification) linked_spec ->
-        forall (c: ConfFlag),
-          (exists n, machine_run n (Executable, initial_state linked_impl) = Some c) ->
+    (** This is the contextual refinement relation
+        A component 'impl' contextually refines 'spec' if,
+        for all contexts 'ctxt'
+        if
+        - 'ctxt' is well formed
+        - 'ctxt' is a valid context to run 'impl'
+        - 'ctxt' linked with 'impl' terminates in a final state 'c'
+          (either HALTED or FAILED)
+        then
+        - 'ctxt' is a valid context to run 'spec'
+        - 'ctxt' linked with 'spce' also terminates in 'c' *)
+    Definition contextual_refinement : relation component_wf :=
+      fun implementation specification: component_wf =>
+      let impl := proj1_sig implementation in
+      let spec := proj1_sig specification in
+      forall (context: m_pre_component) (main:Word) (c: ConfFlag),
+        let linked_impl := make_link_main_lib context impl main in
+        let linked_spec := make_link_main_lib context spec main in
+        let ctxt := Main context main in
+        well_formed_comp e_stk Symbols _ _ word_restrictions ctxt ->
+        is_context ctxt (Lib impl) linked_impl ->
+        (exists n, machine_run n (Executable, initial_state linked_impl) = Some c) ->
+        is_context ctxt (Lib spec) linked_spec /\
           (exists n, machine_run n (Executable, initial_state linked_spec) = Some c).
   End ctxt_ref_def.
 
-  Lemma contextual_reflexive (lib: m_pre_component) (r_stk: RegName) :
-    contextual_refinement lib lib r_stk.
+  Lemma contextual_reflexive : Reflexive contextual_refinement.
   Proof.
-    intros context link1 link2 is_ctxt1 is_ctxt2 c [n run_n].
-    exists n.
-    rewrite <- (context_unique _ _ _ _ _ is_ctxt1 is_ctxt2).
-    apply run_n.
+    intros [impl impl_wf] context main c linked_impl linked_spec ctxt wf_context is_ctxt1 mr.
+    split; assumption.
   Qed.
 
-  Lemma contextual_transitive (lib_a lib_b lib_c : m_pre_component) (r_stk: RegName) :
-    contextual_refinement lib_a lib_b r_stk ->
-    contextual_refinement lib_b lib_c r_stk ->
-    contextual_refinement lib_a lib_c r_stk.
+  Lemma contextual_transitive : Transitive contextual_refinement.
   Proof.
-    intros a_b b_c ctxt link_a link_c is_link_a is_link_c c [n a].
+    intros [lib_a a_wf] [lib_b b_wf] [lib_c c_wf] a_b b_c.
+    intros context main c linked_a linked_c ctxt wf_context is_ctxt_a mr_a.
+    destruct (a_b context main c wf_context is_ctxt_a mr_a) as [is_ctxt_b mr_b].
+    apply (b_c context main c wf_context is_ctxt_b mr_b).
+  Qed.
 
-    destruct ctxt as [ p | ctxt ].
-    { inversion is_link_a.
-    destruct His_program as [link_a' is_prgm].
-    inversion link_a'. rewrite <- H0 in is_prgm.
-    inversion is_prgm. }
-    assert (is_context (Main ctxt w) (Lib lib_b) (make_link_main_lib _ _ _ lib_b ctxt w)).
-    destruct ctxt as [[ms_ctxt imps_ctxt] exp_ctxt].
-    destruct lib_b as [[ms_lib_b imps_lib_b] exp_lib_b].
-    apply make_link_main_lib_is_context.
 
 End contextual_refinement.
