6ab7b42c39f56dd058fb41e69f58a27ffec0f301
Bastien Rousseau <bastien.rousseau@cs.au.dk>
2023-03-03 22:58:18 +0100

    Implementation awkward example with directed capabilities

---
+++ interpreter/lib/scall_dcap.ml
@@ -0,0 +1,193 @@
+
+open Ast
+
+let in_list (e :'a) (l : 'a list) : bool =
+  match (List.find_opt (fun x -> x = e) l) with
+  | None -> false
+  | Some _ -> true
+
+let all_registers : regname list =
+  [PC; STK] @ List.init 32 (fun i -> Reg i)
+
+let rec rclear (regs : regname list) : (statement list) =
+  match regs with
+  | [] -> []
+  | r::regs' -> (Move (r,(CP (Const 0))))::(rclear regs')
+
+let reg_diff (regs : regname list) : regname list=
+  (List.filter (fun r -> not (in_list r regs)) all_registers)
+
+let rclear_inv (regs : regname list) : statement list =
+  (rclear (reg_diff regs))
+
+
+let mclearU : statement list =
+  let mclear_off_end = 9 in
+  let mclear_off_iter = 2 in
+  [
+    Move (Reg 4, Register STK);
+    GetB (Reg 1, Reg 4);
+    GetB (Reg 2, Reg 4);
+    Sub (Reg 2, Register (Reg 1), Register (Reg 2));
+    Lea (Reg 4, Register (Reg 2));
+    GetE (Reg 5, Reg 4);
+    Sub (Reg 5, Register (Reg 5), CP (Const 1));
+    Move (Reg 2, Register PC);
+    Lea (Reg 2, CP (Const mclear_off_end));
+    Move (Reg 3, Register PC);
+    Lea (Reg 3, CP (Const mclear_off_iter));
+    (* iter *)
+    Lt (Reg 6, Register (Reg 5), Register (Reg 1));
+    Jnz (Reg 2, Reg 6);
+    StoreU (Reg 4, CP (Const 0), CP (Const 0));
+    Add (Reg 1, Register (Reg 1), CP (Const 1));
+    Jmp (Reg 3);
+    (* end *)
+  ] @ rclear [Reg 1; Reg 2; Reg 3; Reg 4; Reg 5; Reg 6]
+
+let pushU r = StoreU (STK, CP (Const 0), r)
+let popU r =
+  [ LoadU (r, STK, (CP (Const (-1))));
+    Sub (Reg 1, CP (Const 0), CP (Const 1));
+    Lea (STK, (Register (Reg 1)))
+  ]
+
+let reqglob_instrs r =
+  [ GetL (Reg 1, r);
+    Sub (Reg 1, Register (Reg 1),
+         CP (Const (Z.to_int (Encode.encode_locality Global))));
+    Move (Reg 2, Register PC);
+    Lea (Reg 2, Register (Reg 1));
+    Jnz (Reg 2, Reg 1);
+    Move (Reg 2, Register PC);
+    Lea (Reg 2, CP (Const 4));
+    Jmp (Reg 2);
+    Fail;
+    Move (Reg 1, CP (Const 0));
+    Move (Reg 2, CP (Const 0));
+  ]
+
+let reqperm r (p : Z.t) =
+  [ GetP (Reg 1, r);
+    Sub (Reg 1, Register (Reg 1), CP (Const (Z.to_int p)));
+    Move (Reg 2, Register PC);
+    Lea (Reg 2, CP (Const 6));
+    Jnz (Reg 2, Reg 1);
+    Move (Reg 2, Register PC);
+    Lea (Reg 2, CP (Const 4));
+    Jmp (Reg 2);
+    Fail;
+    Move (Reg 1, CP (Const 0));
+    Move (Reg 1, CP (Const 0))
+  ]
+
+let reqsize r s =
+  [ GetP (Reg 1, r);
+    GetE (Reg 2, r);
+    Sub (Reg 1, Register (Reg 2), Register (Reg 1));
+    Lt (Reg 1, CP (Const s), Register (Reg 1));
+    Move (Reg 2, Register PC);
+    Lea (Reg 2, CP (Const 4));
+    Jnz (Reg 2, Reg 1);
+    Fail
+  ]
+
+
+let prepstack r minsize paramsize =
+  reqperm r (Encode.encode_perm URWLX) @
+  reqsize r (minsize+paramsize) @
+  [ GetB (Reg 1, r);
+    GetA (Reg 2, r);
+    Sub (Reg 2, Register (Reg 2), CP (Const paramsize));
+    Sub (Reg 1, Register (Reg 1), Register (Reg 2));
+    Lea (r, Register (Reg 1));
+    Move (Reg 1, CP (Const 0));
+    Move (Reg 2, CP (Const 0));
+  ]
+
+
+let scall_prologue radv params : (statement list) =
+  let w1 = Encode.encode_statement (Move (Reg 1, Register PC)) in
+  let w2 = Encode.encode_statement (Lea (Reg 1, CP (Const 6))) in
+  let w3 = Encode.encode_statement (Load (STK, Reg 1)) in
+  let w4a = Encode.encode_statement (Sub (Reg 1, CP (Const 0), CP (Const 1))) in
+  let w4b = Encode.encode_statement (LoadU (PC, STK, Register (Reg 1))) in
+  let epilogue_b =
+  [
+    (* Push activation record *)
+    pushU (CP (Const (Z.to_int w1)));
+    pushU (CP (Const (Z.to_int w2)));
+    pushU (CP (Const (Z.to_int w3)));
+    pushU (CP (Const (Z.to_int w4a)));
+    pushU (CP (Const (Z.to_int w4b)));
+  ] in
+
+  let rdiff = reg_diff (params @ [PC;STK;Reg 0;radv]) in
+  let epilogue_e =
+    (* push old pc *)
+    [
+      Move (Reg 1, Register PC) ;
+      Lea (Reg 1, CP (Const (List.length rdiff + 11 + (2 + (2 * (List.length params)))))) ;
+      pushU (Register (Reg 1));
+
+      (* push stack pointer *)
+      (* Note that the stored r_stk will not be able to read itself, *)
+      (*  but that should not be a problem. *)
+      pushU (Register STK);
+      (* set up protected return pointer *)
+      (* since a URWLX capability cannot be made into an E, we have to promote r_t0 first *)
+      Move (Reg 0, Register STK);
+      PromoteU (Reg 0);
+      Lea (Reg 0, CP (Const (-7)));
+      Restrict (Reg 0, CP (Perm  (E, Directed)));
+      (* restrict stack capability *)
+      GetA (Reg 1, STK);
+      GetE (Reg 2, STK);
+      SubSeg (STK, Register (Reg 1), Register (Reg 2))
+    ] @ rclear_inv (params @ [PC;STK;Reg 0;radv])
+  in
+  epilogue_b @ epilogue_e
+
+
+let awkward_example radv =
+
+  let renv = Reg 30 in
+  (* let awkward_epilogue_off = 40 in *)
+
+  reqglob_instrs radv @
+  prepstack STK 10 1 @
+  [ Store (renv, CP (Const 0));
+    (* Local encapsulation *)
+    pushU (Register renv);
+    pushU (Register radv)
+  ]
+  @ scall_prologue radv []
+  @ [
+    pushU (Register (Reg 0));
+    Move (Reg 0, CP (Const 0));
+    Jmp radv;
+    Lea (STK, CP (Const (-6)))
+  ]
+  @ popU radv
+  @ popU renv
+  @ [Store (renv, CP (Const 1));
+     pushU (Register renv);
+    ]
+  @ scall_prologue radv []
+  @ [
+    pushU (Register (Reg 0));
+    Jmp radv;
+    Lea (STK, CP (Const (-6)))
+  ]
+  @ popU renv
+    (* Skip the assert *)
+  @ rclear_inv [PC; (Reg 0)]
+  @ [Jmp (Reg 0)]
+
+let adv_instr = [LoadU (Reg 0, STK, CP (Const (-1))); Jmp (Reg 0)]
+let ret_instr = [Halt]
+
+(* TODO Well, we don't have the return capability to the caller anymore... Ask Aina *)
+(* Also, it sounds that this is not exactly the CC used for the overlay *)
+(*  semantic, cf
+    https://github.com/logsem/cerise-stack-monotone/blob/master/theories/overlay/call.v *)
---
+++ interpreter/src/awkward_dcaps.ml
@@ -0,0 +1,43 @@
+open Libinterp
+open Scall_dcap
+
+let () =
+  let radv = (Ast.Reg 28) in
+  let renv = (Ast.Reg 30) in
+  let prog = awkward_example radv @ adv_instr @ ret_instr in
+
+  (* let addr_max = List.length prog in *)
+  (* let addr_max = (Int32.to_int Int32.max_int)/4096 in *)
+  let addr_max = (Int32.to_int Int32.max_int)/32768 in
+  let module Cfg = struct let addr_max = addr_max end in
+  let module Ui = Interactive_ui.MkUi (Cfg) in
+
+  let m_init_state, m_init_conf =
+    Program.init_machine prog (Some Cfg.addr_max) true Ast.Directed in
+  let adv_upd radv conf =
+    let addr_adv= (List.length (awkward_example radv)) in
+      Machine.upd_reg radv
+        (Machine.Cap (E, Global, addr_adv, addr_adv + (List.length adv_instr), addr_adv))
+        conf
+  in
+  let ret_upd rret conf =
+    let addr_ret= (List.length (awkward_example radv @ adv_instr)) in
+      Machine.upd_reg rret
+        (Machine.Cap (E, Global, addr_ret, addr_ret + (List.length ret_instr), addr_ret))
+        conf
+  in
+  let env_upd renv conf =
+    let addr_data= (List.length prog) in
+      Machine.upd_reg renv
+        (Machine.Cap (RW, Global, addr_data, addr_data + 1, addr_data))
+        conf
+  in
+  let m_init_conf = env_upd renv (ret_upd (Reg 0) (adv_upd radv m_init_conf)) in
+  let m_init = ( m_init_state, m_init_conf) in
+
+  (* let term = Term.create () in *)
+
+  let prog_panel_start = ref 0 in
+  let stk_panel_start = ref ((Cfg.addr_max)/2) in
+
+  Ui.render_loop prog_panel_start stk_panel_start m_init
