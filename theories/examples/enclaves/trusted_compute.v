c90774e18c787cbe782b175d191c1cf743f842a1
June Rousseau <june.rousseau@cs.au.dk>
2024-10-30 16:12:50 +0100

    Fix code `trusted_compute` and WIP specification `callback`

--- theories/examples/enclaves/trusted_compute.v
+++ theories/examples/enclaves/trusted_compute.v
@@ -64,7 +64,10 @@ Section trusted_compute_example.
         (* sanity check: w_res is a sealed capability *)
         GetOType r_t2 r_t0;
         Sub r_t2 r_t2 (-1)%Z;
-        Jnz r_t3 r_t2;
+        Mov r_t5 PC ;
+        Lea r_t5 4 ;
+        Jnz r_t5 r_t2;
+        Jmp r_t3;
 
         (* check otype(w_res) against identity of the enclave *)
         GetOType r_t2 r_t0;
@@ -73,7 +76,10 @@ Section trusted_compute_example.
         Jnz r_t3 r_t2;
         Load r_t4 r_t4;
         Sub r_t4 r_t4 hash_enclave;
-        Jnz r_t3 r_t4;
+        Mov r_t5 PC ;
+        Lea r_t5 4 ;
+        Jnz r_t5 r_t4;
+        Jmp r_t3;
 
         (* get returned value and assert it to be 42 *)
         UnSeal r_t0 r_t0 r_t1;
@@ -190,7 +196,7 @@ Section trusted_compute_example.
     rewrite decode_encode_perm_inv.
     iGo "Hcode".
     (* FIXME: not sure why I need to rewrite this *)
-    replace (a_data ^+ 1)%a with (b_main ^+ 44)%a by solve_addr'.
+    replace (a_data ^+ 1)%a with (b_main ^+ 50)%a by solve_addr'.
     iGo "Hcode".
     iApply (wp_wand with "[-]"); last (iIntros (v) "H"; by iLeft).
     iApply "Hφ".
@@ -207,12 +213,14 @@ Section trusted_compute_example.
   (* Linking table invariant *)
   Definition link_tableN := (trusted_computeN.@"link_table").
   Definition link_table_inv
-    table_addr v_table_addr
-    b_link e_link a_link v_link
+    (* table_addr v_table_addr *)
+    (* b_link e_link a_link v_link *)
+    v_link
     assert_entry b_assert e_assert v_assert :=
     na_inv logrel_nais link_tableN
-      ((table_addr, v_table_addr) ↦ₐ LCap RO b_link e_link a_link v_link
-       ∗ (assert_entry, v_link) ↦ₐ LCap E b_assert e_assert b_assert v_assert)%I.
+      (* ((table_addr, v_table_addr) ↦ₐ LCap RO b_link e_link a_link v_link *)
+       (* ∗ *)
+         ((assert_entry, v_link) ↦ₐ LCap E b_assert e_assert b_assert v_assert)%I.
 
   (* Assert invariant *)
   Definition assertN := (trusted_computeN.@"assert").
@@ -231,58 +239,131 @@ Section trusted_compute_example.
     (b_link a_link e_link assert_entry : Addr) (* linking *)
     (assert_lt_offset : Z)
     (b_assert e_assert a_flag : Addr) (v_assert : Version) (* assert *)
-    (w0 w1 w2 w3 : LWord)
+    (w0 w1 w2 w3 w4 w5 : LWord)
     φ :
 
     let v_link := pc_v in
     let link_cap := LCap RO b_link e_link a_link v_link in
 
-    let a_code_main := (b_code_main ^+ 1)%a in
-    let e_data_main := (b_data_main ^+ trusted_compute_main_data_len)%a in
+    let e_main := (b_main ^+ trusted_compute_main_len)%a in
+    let a_callback := (b_main ^+ trusted_compute_main_init_len)%a in
+    let a_data := (b_main ^+ trusted_compute_main_code_len)%a in
 
     let trusted_compute_main := trusted_compute_main_code assert_lt_offset in
-    let len_main_code := length trusted_compute_main in
-    let main_code_end := (a_code_main ^+ len_main_code)%a in
-    let main_end := (main_code_end ^+ trusted_compute_main_data_len)%a in
-    let link_addr := b_data_main in
+    ContiguousRegion b_main trusted_compute_main_len ->
 
 
     (a_link + assert_lt_offset)%a = Some assert_entry →
     (* TODO assume that, between 'main_end' and 'end_adv',
        there is only integers (for simplicity) *)
-    main_end < end_adv ->
+    (* main_end < end_adv -> *)
 
     (* TODO not needed for this part of the spec, but will be required for the full proof *)
-    (link_table_inv link_addr v_link
-      b_link e_link a_link v_link assert_entry
-      b_assert e_assert v_assert
+    (link_table_inv
+       (* link_addr v_link *)
+       (* b_link e_link a_link *)
+       v_link
+       assert_entry b_assert e_assert v_assert
     ∗ assert_inv b_assert a_flag e_assert v_assert
     ∗ flag_inv a_flag v_assert)
 
     ⊢ ((
-          codefrag a_code_main pc_v trusted_compute_main
-          ∗ (b_code_main, pc_v) ↦ₐ (LCap RW b_data_main e_data_main b_data_main pc_v)
-          ∗ [[b_data_main, e_data_main]] ↦ₐ{pc_v} [[ trusted_compute_data link_cap  ]]
+          codefrag b_main pc_v trusted_compute_main
+          ∗ ((a_data)%a, pc_v) ↦ₐ link_cap
+          ∗ ((a_data ^+ 1)%a, pc_v) ↦ₐ (LCap RWX b_main e_main a_data pc_v)
 
-          ∗ PC ↦ᵣ LCap RWX b_code_main end_adv a_code_main pc_v
+          ∗ PC ↦ᵣ LCap RX b_main e_main a_callback pc_v
           ∗ r_t0 ↦ᵣ w0
           ∗ r_t1 ↦ᵣ w1
           ∗ r_t2 ↦ᵣ w2
           ∗ r_t3 ↦ᵣ w3
           ∗ r_t4 ↦ᵣ w4
+          ∗ r_t5 ↦ᵣ w5
           (* NOTE this post-condition stops after jumping to the adversary *)
-          ∗ ▷ ( codefrag a_code_main pc_v trusted_compute_main
-                ∗ (b_code_main, pc_v) ↦ₐ (LCap RW b_data_main e_data_main b_data_main pc_v)
-                ∗ [[b_data_main, e_data_main]] ↦ₐ{pc_v} [[ trusted_compute_data link_cap  ]]
+          ∗ ▷ ( codefrag b_main pc_v trusted_compute_main
+                ∗ ((a_data)%a, pc_v) ↦ₐ link_cap
+                ∗ ((a_data ^+ 1)%a, pc_v) ↦ₐ (LWInt 0%Z)
 
-                ∗ PC ↦ᵣ (LCap RWX main_end end_adv main_end pc_v)
-                ∗ r_t0 ↦ᵣ (LCap E b_code_main end_adv main_end pc_v)
-                ∗ r_t1 ↦ᵣ (LCap RWX main_end end_adv main_end pc_v)
+                ∗ PC ↦ᵣ LCap RWX b_main e_main a_data pc_v
+                ∗ r_t0 ↦ᵣ LInt 0
+                ∗ r_t1 ↦ᵣ LInt 0
                 ∗ r_t2 ↦ᵣ LInt 0
                 ∗ r_t3 ↦ᵣ LInt 0
                 ∗ r_t4 ↦ᵣ LInt 0
+                ∗ r_t5 ↦ᵣ LInt 0
                   -∗ WP Seq (Instr Executable) {{ φ }}))
          -∗ WP Seq (Instr Executable) {{ λ v, φ v ∨ ⌜v = FailedV⌝ }})%I.
   Proof.
+
+    (* We define a local version of solve_addr, which subst and unfold every computed addresses  *)
+    Local Tactic Notation "solve_addr'" :=
+      repeat (lazymatch goal with x := _ |- _ => subst x end)
+      ; repeat (match goal with
+                  | H: ContiguousRegion _ _  |- _ =>
+                      rewrite /ContiguousRegion /trusted_compute_main_len in H
+                end)
+      ; rewrite !/trusted_compute_main_code_len /trusted_compute_main_len
+          /trusted_compute_main_init_len /trusted_compute_main_callback_len
+      ; solve_addr.
+
+
+    intros ?????? Hregion Hassert.
+    iIntros "[HlinkInv HassertInv]
+             (Hcode & Hlink_cap & Hdata1 & HPC & Hr0 & Hr1 & Hr2 & Hr3 & Hr4 & Hr5 & Hcont)".
+    codefrag_facts "Hcode".
+    iInstr "Hcode". (* Mov *)
+    iInstr "Hcode". (* Mov *)
+    iInstr "Hcode". (* Lea *)
+
+    iInstr "Hcode". (* Lea *)
+    replace (a_data ^+1)%a with (b_main ^+ 50)%a by solve_addr'.
+    iInstr "Hcode". (* Load *)
+    iInstr "Hcode". (* Load *)
+    { admit. (* TODO it's fine *) }
+    iInstr "Hcode". (* Lea *)
+
+
+    destruct (is_sealedL w0) eqn:Hsealed_w0 ; cycle 1.
+    { (* w0 is not a sealed  *)
+      destruct_lword (w0) ; cbn in Hsealed_w0 ; try done.
+      all: iInstr "Hcode". (* GetOType *)
+      all: iInstr "Hcode". (* Sub *)
+      all: replace (-1 - -1) with 0 by lia.
+      all: iInstr "Hcode". (* Mov *)
+      all: iInstr "Hcode". (* Lea *)
+      all: iInstr "Hcode". (* Jnz *)
+      all: iInstr "Hcode". (* Jmp *)
+      all: iInstr "Hcode". (* Fail *)
+      all: by (wp_end; iRight).
+    }
+
+    destruct w0 as [ ? | ? | o sw0 ]
+    ; cbn in Hsealed_w0 ; try done; clear Hsealed_w0.
+    iInstr "Hcode". (* GetOType *)
+    iInstr "Hcode". (* Sub *)
+    replace (o - -1) with (o + 1) by lia.
+    assert (Ho : LInt (o + 1) ≠ LInt 0) by (clear ; intro ; inversion H ; solve_finz).
+    iInstr "Hcode". (* Mov *)
+    iInstr "Hcode". (* Lea *)
+    iInstr "Hcode". (* Jnz *)
+    iInstr "Hcode". (* GetOType *)
+    (* iInstr "Hcode". (* EStoreId r_t2 r_t2 r_t4 *) *)
+  Admitted.
+
+        (* (* check otype(w_res) against identity of the enclave *) *)
+        (* GetOType r_t2 r_t0; (* r2 := LInt σ *) *)
+        (* EStoreId r_t2 r_t2 r_t4; (* r2 := if etbl[σ] = Some I then 1 else 0 *) *)
+        (* Sub r_t2 r_t2 1; (* r2 := if etbl[σ] = Some I then 0 else -1 *) *)
+        (* Jnz r_t3 r_t2; (* Jumps to `fails` if (etbl[σ] = None)  *) *)
+        (* Load r_t4 r_t4; *)
+        (* Sub r_t4 r_t4 hash_enclave; *)
+        (* Jnz r_t3 r_t4; *)
+
+        (* (* get returned value and assert it to be 42 *) *)
+        (* UnSeal r_t0 r_t0 r_t1; *)
+        (* Mov r_t1 r_t5; *)
+        (* GetA r_t4 r_t0; *)
+        (* Mov r_t5 42%Z *)
+
 
 End trusted_compute_example.
