29331a4757b6a70c7e2d89c4a2b306e63187902f
Denis Carnier <decrn@users.noreply.github.com>
2025-03-25 15:50:42 +0100

    More WIP WP rules

--- theories/rules/rules_EInit.v
+++ theories/rules/rules_EInit.v
@@ -44,6 +44,7 @@ Section cap_lang_rules.
     | EInit_success_no_revoke code_b code_e code_a code_v data_b data_e data_a data_v vmap :
       incrementLPC lregs = Some lregs' → (* PC can be incremented *)
       n + 1 = n' → (* n is the value of the EC register, it can be incremented to n' *)
+      tidx = n →
       eid = hash_lmemory_region lmem code_b code_e code_v → (* the hashed value `eid` corresponds to hashing the code capability's memory footprint *)
       (ot + 2)%ot = Some (ot ^+ 2)%ot → (* the otype can be incremented *)
       lregs !! rs = Some (LCap RX code_b code_e code_a code_v) → (* rs contains a valid capability *)
@@ -59,9 +60,6 @@ Section cap_lang_rules.
       EInit_fail lregs lmem ot →
       EInit_spec lregs lregs' lmem lmem' n n' tidx eid ot rs FailedV.
 
-  (* TODO: Failure lmem does not contain none at code_b etc *)
-  (* TODO: how to determine tidx *)
-  (* TODO @Denis *)
   Lemma wp_einit E pc_p pc_b pc_e pc_a pc_v pc_a' lw
     lregs lmem tidx eid rs n :
     decodeInstrWL lw = EInit rs →
@@ -69,11 +67,11 @@ Section cap_lang_rules.
     (pc_a + 1)%a = Some pc_a' →
     regs_of (EInit rs) ⊆ dom lregs →
 
-    {{{ ▷ [∗ map] k↦y ∈ lregs, k ↦ᵣ y ∗
-        ▷ [∗ map] la↦lw ∈ lmem, la ↦ₐ lw ∗
-        PC ↦ᵣ LCap pc_p pc_b pc_e pc_a pc_v ∗ (pc_a, pc_v) ↦ₐ lw ∗
-        EC⤇ n (* need a fragment for the EC register *)
-        }}}
+    {{{ (▷ [∗ map] k↦y ∈ lregs, k ↦ᵣ y) ∗
+        (▷ [∗ map] la↦lw ∈ lmem, la ↦ₐ lw) ∗
+        PC ↦ᵣ LCap pc_p pc_b pc_e pc_a pc_v ∗
+        (pc_a, pc_v) ↦ₐ lw ∗
+        EC⤇ n (* need a fragment for the EC register *) }}}
       Instr Executable @ E
     {{{ lregs' lmem' retv n' ot, RET retv;
         ⌜ EInit_spec lregs lregs' lmem lmem' n n' tidx eid ot rs retv⌝ ∗
@@ -81,34 +79,35 @@ Section cap_lang_rules.
         PC ↦ᵣ LCap pc_p pc_b pc_e pc_a' pc_v ∗
         [∗ map] la↦lw ∈ lmem', la ↦ₐ lw ∗
         [∗ map] k↦y ∈ lregs', k ↦ᵣ y ∗
-        EC⤇ n' ∗ (* need to give back the fragment of EC *)
+        EC⤇ n+1 ∗ (* need to give back the fragment of EC *)
         if decide (retv = NextIV) then (* if EInit was successful *)
           (* gain a non-duplicable token that asserts ownership over the enclave at etable index `tidx` *)
           enclave_cur tidx eid
           (* @TODO Denis/June: seal predicates needed? *)
         else emp }}}.
   Proof.
-   (*  iIntros (Hinstr Hvpc φ) "[Hpc Hpca] Hφ". *)
-   (*  apply isCorrectLPC_isCorrectPC_iff in Hvpc; cbn in Hvpc. *)
-   (*  iApply wp_lift_atomic_head_step_no_fork; auto. *)
-   (*  iIntros (σ1 ns l1 l2 nt) "Hσ1 /=". destruct σ1; simpl. *)
-   (*  iDestruct "Hσ1" as (lr lm vmap) "(Hr & Hm & %HLinv)"; simpl in HLinv. *)
-   (*  iDestruct (@gen_heap_valid with "Hr Hpc") as %?. *)
-   (*  iDestruct (@gen_heap_valid with "Hm Hpca") as %?. *)
-   (*  iModIntro. *)
-   (*  iSplitR. by iPureIntro; apply normal_always_head_reducible. *)
-   (*  iIntros (e2 σ2 efs Hstep). *)
-   (*  apply prim_step_exec_inv in Hstep as (-> & -> & (c & -> & Hstep)). *)
-   (*  iNext; iIntros "_". *)
-   (*  iSplitR; auto. eapply step_exec_inv in Hstep; eauto. *)
-   (*  2: rewrite -/((lword_get_word (LCap pc_p pc_b pc_e pc_a pc_v))) *)
-   (*  ; eapply state_corresponds_reg_get_word ; eauto. *)
-   (*  2: eapply state_corresponds_mem_correct_PC ; eauto; cbn ; eauto. *)
-   (*  cbn in Hstep; simplify_eq. *)
-   (*  iModIntro. *)
-   (*  iSplitR "Hφ Hpc Hpca"; last (iApply "Hφ" ; iFrame). *)
-   (*  iExists lr, lm, vmap. *)
-   (*  iFrame; auto. *)
+    iIntros (Hinstr Hvpc Hpc Hdomi φ) "(Hregs & Hmem & Hpcc & Hpclw & Hec) Hφ".
+    apply isCorrectLPC_isCorrectPC_iff in Hvpc; cbn in Hvpc.
+    iApply wp_lift_atomic_head_step_no_fork; auto.
+    iIntros (σ1 ns l1 l2 nt) "Hσ1 /=". destruct σ1; simpl.
+    iDestruct "Hσ1" as (lr lm vmap) "(% & % & % & Hr & Hm & %Hcur & Henc_cur & Henc_prev & Henc_all & HEC & %Hdisj & %Hdom & %Hdisj2 & %Union_all & %Hstate)".
+    iDestruct (@gen_heap_valid with "Hr Hpcc") as %?.
+    iDestruct (@gen_heap_valid with "Hm Hpclw") as %?.
+    iModIntro.
+    iSplitR. by iPureIntro; apply normal_always_head_reducible.
+    iIntros (e2 σ2 efs Hstep).
+    apply prim_step_exec_inv in Hstep as (-> & -> & (c & -> & Hstep)).
+    iNext; iIntros "_".
+    iSplitR; auto. eapply step_exec_inv in Hstep; eauto.
+    2: rewrite -/((lword_get_word (LCap pc_p pc_b pc_e pc_a pc_v)))
+    ; eapply state_corresponds_reg_get_word ; eauto.
+    2: eapply state_corresponds_mem_correct_PC ; eauto; cbn ; eauto.
+    cbn in Hstep; simplify_eq.
+    iModIntro.
+    iSplitR "Hφ Hpcc Hpclw".
+    iExists lr, lm, vmap.
+    iFrame; auto.
+    all: admit.
    (* Qed. *)
   Admitted.
 
--- theories/rules/rules_EStoreId.v
+++ theories/rules/rules_EStoreId.v
@@ -18,54 +18,53 @@ Section cap_lang_rules.
   Implicit Types mem : Mem.
   Implicit Types lmem : LMem.
 
-  Inductive EStoreId_spec : cap_lang.val -> Prop :=
+  Inductive EStoreId_spec (lregs lregs' : LReg) (lmem lmem' : LMem) otype tidx : cap_lang.val -> Prop :=
   | EStoreId_spec_success:
-    EStoreId_spec NextIV
+    has_seal otype tidx → (* we associate a given table index with the provided otype *)
+    EStoreId_spec lregs lregs' lmem lmem' otype tidx NextIV
   |EStoreId_spec_failure:
-    EStoreId_spec FailedV.
+    EStoreId_spec lregs lregs' lmem lmem' otype tidx FailedV.
 
   (* TODO @Denis *)
   (* The EStoreId instruction fetches the machine's stored hash for a given OType.
      It is used by the client of an enclave to verify that a value signed by a given OType originates from code with a known hash `I`. *)
   (* Logically, the crux of this contract is that the post-condition contains a duplicable resource `enclave_all`,
-     which states that an enclave has existed at some point in the past at some index `tid` in the enclave table, and that this index corresponds to some hash/EIdentity `I` *)
+     which states that an enclave has existed at some point in the past at some index `tidx` in the enclave table, and that this index corresponds to some hash/EIdentity `I` *)
   (* Essentially it gives us a partial view on the enclave table, since we now know that at a particular index, at some point, there was an enclave with a particular identity. *)
-  (* In a later step of the verification, we will trade this resource for the specific predicate that always holds for results signed by enclaves with that hash... *)
+  (* In a later step of the verification, an invariant will allow us to trade this resource for the specific predicate that always holds for results signed by enclaves with that hash... *)
   (* This enables "learning" some information about the signed, yet unknown result: we will be able to establish that at least the above predicate will hold for it... *)
-  Lemma wp_estoreid_success_unknown E pc_p pc_b pc_e pc_a pc_a' pc_v lw rd rs otype any :
+  Lemma wp_estoreid E pc_p pc_b pc_e pc_a pc_a' pc_v lw rd rs otype any lregs lmem :
     decodeInstrWL lw = EStoreId rd rs →
     isCorrectLPC (LCap pc_p pc_b pc_e pc_a pc_v) →
     (pc_a + 1)%a = Some pc_a' →
-    {{{ PC ↦ᵣ LCap pc_p pc_b pc_e pc_a pc_v
-        ∗ (pc_a, pc_v) ↦ₐ lw
-        ∗ rs ↦ᵣ LWInt otype
-        ∗ rd ↦ᵣ any
-    }}}
+    {{{ (▷ [∗ map] k↦y ∈ lregs, k ↦ᵣ y) ∗
+        (▷ [∗ map] la↦lw ∈ lmem, la ↦ₐ lw) ∗
+        PC ↦ᵣ LCap pc_p pc_b pc_e pc_a pc_v ∗
+        (pc_a, pc_v) ↦ₐ lw ∗
+        rs ↦ᵣ LWInt otype ∗
+        rd ↦ᵣ any }}}
       Instr Executable @ E
-      {{{ retv, RET retv;
-          (pc_a, pc_v) ↦ₐ lw
-          ∗ rs ↦ᵣ LWInt otype
-          ∗ ((⌜ retv = NextIV ⌝
-              ∗ PC ↦ᵣ LCap pc_p pc_b pc_e pc_a' pc_v
-              ∗ ∃ (I : EIdentity), ∃ (tid : TIndex),
-                  rd ↦ᵣ (LWInt I)
-                  ∗ (enclave_all tid I)
-                  ∗ ⌜ has_seal otype tid ⌝
-             )
-             ∨
-               (⌜ retv = FailedV ⌝
-                ∗ PC ↦ᵣ LCap pc_p pc_b pc_e pc_a pc_v
-                ∗ rd ↦ᵣ any
-               )
-            )
-
-    }}}.
+    {{{ lregs' lmem' tidx retv, RET retv;
+        ⌜ EStoreId_spec lregs lregs' lmem lmem' otype tidx retv⌝ ∗
+        (pc_a, pc_v) ↦ₐ lw ∗
+        rs ↦ᵣ LWInt otype ∗
+        [∗ map] k↦y ∈ lregs', k ↦ᵣ y ∗
+        [∗ map] la↦lw ∈ lmem', la ↦ₐ lw ∗
+        if decide (retv = NextIV) then
+          PC ↦ᵣ LCap pc_p pc_b pc_e pc_a' pc_v ∗
+          ∃ (I : EIdentity),
+            rd ↦ᵣ (LWInt I) ∗
+            enclave_all tidx I (*!*)
+        else
+          PC ↦ᵣ LCap pc_p pc_b pc_e pc_a pc_v ∗
+          rd ↦ᵣ any }}}.
   Proof.
     iIntros (Hinstr Hvpc Hpca' φ) "(Hpc & Hpca & Hrs & Hrd) Hφ".
     apply isCorrectLPC_isCorrectPC_iff in Hvpc; cbn in Hvpc.
     iApply wp_lift_atomic_head_step_no_fork; auto.
-    iIntros (σ1 ns l1 l2 nt) "Hσ1 /=". destruct σ1; simpl.
-    (* iDestruct "Hσ1" as (lr lm vmap) "(Hr & Hm & %HLinv)"; simpl in HLinv. *)
+    iIntros (σ1 ns l1 l2 nt) "Hσ1 /=".
+    iDestruct "Hσ1" as (lr lm vmap) "(% & % & % & Hr & Hm & %Hcur & Henc_cur & Henc_prev & Henc_all & HEC & %Hdisj & %Hdom & %Hdisj2 & %Union_all & %State)".
+    all: admit.
     (* iDestruct (@gen_heap_valid with "Hr Hpc") as %?. *)
     (* iDestruct (@gen_heap_valid with "Hm Hpca") as %?. *)
     (* iModIntro. *)
@@ -73,9 +72,9 @@ Section cap_lang_rules.
     (* iIntros (e2 σ2 efs Hstep). *)
     (* apply prim_step_exec_inv in Hstep as (-> & -> & (c & -> & Hstep)). *)
     (* iNext; iIntros "_". *)
-    (* iSplitR; auto. eapply step_exec_inv in Hstep; eauto. *)
-    (* 2: rewrite -/((lword_get_word (LCap pc_p pc_b pc_e pc_a pc_v))) *)
-    (* ; eapply state_corresponds_reg_get_word ; eauto. *)
+    (* iSplitR; auto. *)
+    (* iModIntro. *)
+    (* rewrite -/((lword_get_word (LCap pc_p pc_b pc_e pc_a pc_v))). *)
     (* 2: eapply state_corresponds_mem_correct_PC ; eauto; cbn ; eauto. *)
     (* cbn in Hstep; simplify_eq. *)
     (* iModIntro. *)
