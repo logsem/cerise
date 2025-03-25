a0f7ca934b45cdfd6ade0e315ee24e07090a4274
Denis Carnier <decrn@users.noreply.github.com>
2025-03-25 19:16:12 +0100

    Cleanup WP EInit, no proof

--- theories/rules/rules_EInit.v
+++ theories/rules/rules_EInit.v
@@ -40,60 +40,71 @@ Section cap_lang_rules.
     | EInit_fail_dcap_no_rw :
       EInit_fail lregs lmem ot.
 
-  Inductive EInit_spec (lregs lregs' : LReg) (lmem lmem' : LMem) (n n' : nat) (tidx : TIndex) (eid : EIdentity) (ot : OType) (rs : RegName) : cap_lang.val → Prop :=
+  Inductive EInit_spec (lregs lregs' : LReg) (lmem lmem' : LMem) (tidx tidx_incr : TIndex) (eid : EIdentity) (ot : OType) (rs : RegName) : cap_lang.val → Prop :=
     | EInit_success_no_revoke code_b code_e code_a code_v data_b data_e data_a data_v :
       incrementLPC lregs = Some lregs' → (* PC can be incremented *)
-      n + 1 = n' → (* n is the value of the EC register, it can be incremented to n' *)
-      tidx = n →
-      eid = hash_lmemory_region lmem code_b code_e code_v → (* the hashed value `eid` corresponds to hashing the code capability's memory footprint *)
-      (ot + 2)%ot = Some (ot ^+ 2)%ot → (* the otype can be incremented *)
+      tidx + 1 = tidx_incr →
+      eid = hash_lmemory_region lmem code_b code_e code_v → (* eid = hash(ccap) *)
+      (ot + 2)%ot = Some (ot ^+ 2)%ot → (* there are still otypes left in the pool *)
       lregs !! rs = Some (LCap RX code_b code_e code_a code_v) → (* rs contains a valid capability *)
       lmem !! (code_b, code_v) = Some (LCap RW data_b data_e data_a data_v) → (* the base address of the code capability points to a valid data capability *)
-      is_valid_updated_lmemory lmem (finz.seq_between code_b code_e) code_v lmem' → (* support revocation *) (* all memory in the code capability is "current" w.r.t. revocation *)
-      is_valid_updated_lmemory lmem (finz.seq_between data_b data_e) data_v lmem' → (* support revocation *) (* all memory in the data capability is "current" w.r.t. revocation *)
-      unique_in_registersL lregs (Some rs) (LCap RX code_b code_e code_a code_v) → (* the code capability in the rs register is unique across all other registers *)
-      unique_in_registersL lregs None (* no excluded register this time *) (LCap RW data_b data_e data_a data_v) → (* the data capability in the rs register is unique across all other registers *)
-      (* TODO: missing above: need to exclude the capability at b itself, cannot just sweep all of memory *)
+      is_valid_updated_lmemory lmem (finz.seq_between code_b code_e) code_v lmem' → (* all memory in the code capability is "current" w.r.t. revocation *)
+      is_valid_updated_lmemory lmem (finz.seq_between data_b data_e) data_v lmem' → (* all memory in the data capability is "current" w.r.t. revocation *)
+      unique_in_registersL lregs (Some rs) (LCap RX code_b code_e code_a code_v) → (* the code capability is unique across all registers (except where it is stored: in `rs`) *)
+      unique_in_registersL lregs None      (LCap RW data_b data_e data_a data_v) → (* the data capability is unique across all registers *)
       incrementLPC (<[ rs := next_version_lword (LCap E code_b code_e (code_b ^+ 1)%a code_v)]> lregs) = Some lregs' → (* the pc will be incremented and rs will point to a "current" sentry capability *)
-      EInit_spec lregs lregs' lmem lmem' n n' tidx eid ot rs NextIV
+      EInit_spec lregs lregs' lmem lmem' tidx tidx_incr eid ot rs NextIV
     | EInit_failure :
-      n = n' →
+      tidx = tidx_incr →
       EInit_fail lregs lmem ot →
-      EInit_spec lregs lregs' lmem lmem' n n' tidx eid ot rs FailedV.
+      EInit_spec lregs lregs' lmem lmem' tidx tidx_incr eid ot rs FailedV.
 
   Lemma wp_einit E pc_p pc_b pc_e pc_a pc_v pc_a' lw
-    lregs lmem tidx eid rs n :
+    lregs lmem tidx eid rs :
     decodeInstrWL lw = EInit rs →
     isCorrectLPC (LCap pc_p pc_b pc_e pc_a pc_v) →
     (pc_a + 1)%a = Some pc_a' →
     regs_of (EInit rs) ⊆ dom lregs →
-
     {{{ (▷ [∗ map] k↦y ∈ lregs, k ↦ᵣ y) ∗
         (▷ [∗ map] la↦lw ∈ lmem, la ↦ₐ lw) ∗
         PC ↦ᵣ LCap pc_p pc_b pc_e pc_a pc_v ∗
         (pc_a, pc_v) ↦ₐ lw ∗
-        EC⤇ n (* need a fragment for the EC register *) }}}
+        EC⤇ tidx }}}
       Instr Executable @ E
-    {{{ lregs' lmem' retv n' ot, RET retv;
-        ⌜ EInit_spec lregs lregs' lmem lmem' n n' tidx eid ot rs retv⌝ ∗
+    {{{ lregs' lmem' retv tidx_incr ot, RET retv;
+        ⌜ EInit_spec lregs lregs' lmem lmem' tidx tidx_incr eid ot rs retv⌝ ∗
         (pc_a, pc_v) ↦ₐ lw ∗
         PC ↦ᵣ LCap pc_p pc_b pc_e pc_a' pc_v ∗
         [∗ map] la↦lw ∈ lmem', la ↦ₐ lw ∗
         [∗ map] k↦y ∈ lregs', k ↦ᵣ y ∗
-        EC⤇ n+1 ∗ (* need to give back the fragment of EC *)
         if decide (retv = NextIV) then (* if EInit was successful *)
-          (* gain a non-duplicable token that asserts ownership over the enclave at etable index `tidx` *)
-          enclave_cur tidx eid
+          EC⤇ tidx+1 ∗ (* EC only increments if EInit was successful... *)
+          enclave_cur tidx eid (* non-dup token asserting ownership over the enclave at etable index `tidx` *)
           (* @TODO Denis/June: seal predicates needed? *)
-        else emp }}}.
+        else
+          EC⤇ tidx (* unchanged *) }}}.
   Proof.
-    iIntros (Hinstr Hvpc Hpc Hdomi φ) "(Hregs & Hmem & Hpcc & Hpclw & Hec) Hφ".
+    iIntros (Hinstr Hvpc HPC Dregs φ) "(>Hregs & >Hmem & HPCCap & HPCv & HECv) Hφ".
     apply isCorrectLPC_isCorrectPC_iff in Hvpc; cbn in Hvpc.
     iApply wp_lift_atomic_head_step_no_fork; auto.
     iIntros (σ1 ns l1 l2 nt) "Hσ1 /=". destruct σ1; simpl.
-    iDestruct "Hσ1" as (lr lm vmap) "(% & % & % & Hr & Hm & %Hcur & Henc_cur & Henc_prev & Henc_all & HEC & %Hdisj & %Hdom & %Hdisj2 & %Union_all & %Hstate)".
-    iDestruct (@gen_heap_valid with "Hr Hpcc") as %?.
-    iDestruct (@gen_heap_valid with "Hm Hpclw") as %?.
+    iDestruct "Hσ1" as (lr lm vmap tbl_cur tbl_prev tbl_all)
+                         "(Hr & Hm
+                          & -> & Htbl_cur & Htbl_prev & Htbl_all
+                          & HEC
+                          & %Hdom_tbl1 & %Hdom_tbl2 & %Hdom_tbl3 & %Hdom_tbl4
+                          & %HLinv)"
+    ; cbn in HLinv, Hdom_tbl1, Hdom_tbl2, Hdom_tbl3, Hdom_tbl4.
+
+    iDestruct (gen_heap_valid_inclSepM with "Hr Hregs") as %Hregs.
+    (* have Hregs_pc := lookup_weaken _ _ _ _ HPC Hregs. *)
+    (* specialize (indom_lregs_incl _ _ _ Dregs Hregs) as Hri. unfold regs_of in Hri. *)
+    (* odestruct (Hri rs) as [lsrcv [Hlsrc' Hlsrc] ]; first by set_solver+. *)
+
+    (* Derive necessary memory *)
+    iDestruct (gen_heap_valid_inclSepM with "Hm Hmem") as %Hmem.
+    (* iDestruct (gen_mem_valid_inSepM lmem lm with "Hm Hmem") as %Hma; eauto. *)
+
     iModIntro.
     iSplitR. by iPureIntro; apply normal_always_head_reducible.
     iIntros (e2 σ2 efs Hstep).
@@ -102,10 +113,10 @@ Section cap_lang_rules.
     iSplitR; auto. eapply step_exec_inv in Hstep; eauto.
     2: rewrite -/((lword_get_word (LCap pc_p pc_b pc_e pc_a pc_v)))
     ; eapply state_corresponds_reg_get_word ; eauto.
-    2: eapply state_corresponds_mem_correct_PC ; eauto; cbn ; eauto.
+    (* 2: eapply state_corresponds_mem_correct_PC ; eauto; cbn ; eauto. *)
     cbn in Hstep; simplify_eq.
     iModIntro.
-    iSplitR "Hφ Hpcc Hpclw".
+    iSplitR "Hφ HECv". (* not yet sure what is needed *)
     iExists lr, lm, vmap.
     iFrame; auto.
     all: admit.
