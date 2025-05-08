14aa1abced36997260cab42f715477b488f5a5db
June Rousseau <june.rousseau@cs.au.dk>
2025-05-08 13:07:31 +0200

    FTLR hash

--- theories/ftlr/Hash.v
+++ theories/ftlr/Hash.v
@@ -22,34 +22,34 @@ Section fundamental.
     (lw : LWord) (dst src : RegName)  (P:D) :
     ftlr_instr lregs p b e a v lw (Hash dst src) P.
   Proof.
-    (* intros Hp Hsome i Hbae Hi. *)
-    (* iIntros "#IH #Hinv #Hinva #Hreg #[Hread Hwrite] Hown Ha HP Hcls HPC Hmap". *)
-    (* rewrite delete_insert_delete. *)
-    (* iDestruct ((big_sepM_delete _ _ PC) with "[HPC Hmap]") as "Hmap /="; *)
-    (*   [apply lookup_insert|rewrite delete_insert_delete;iFrame|]. simpl. *)
-    (* iApply (wp_Hash with "[$Ha $Hmap]"); eauto. *)
-    (* { by simplify_map_eq. } *)
-    (* { rewrite /subseteq /map_subseteq /set_subseteq_instance. intros rr _. *)
-    (*   apply elem_of_dom. apply lookup_insert_is_Some'; eauto. } *)
+    intros Hp Hsome i Hbae Hi.
+    iIntros "#IH #Hinv #Hinva #Hreg #[Hread Hwrite] Hown Ha HP Hcls HPC Hmap".
+    rewrite delete_insert_delete.
+    iDestruct ((big_sepM_delete _ _ PC) with "[HPC Hmap]") as "Hmap /=";
+      [apply lookup_insert|rewrite delete_insert_delete;iFrame|]. simpl.
+    iApply (wp_hash with "[$Ha $Hmap]"); eauto.
+    { by simplify_map_eq. }
+    { rewrite /subseteq /map_subseteq /set_subseteq_instance. intros rr _.
+      apply elem_of_dom. apply lookup_insert_is_Some'; eauto. }
 
-    (* iIntros "!>" (regs' retv). iDestruct 1 as (HSpec) "[Ha Hmap]". *)
-    (* destruct HSpec; cycle 1. *)
-    (* { iApply wp_pure_step_later; auto. iMod ("Hcls" with "[HP Ha]");[iExists lw;iFrame|iModIntro]. iNext. *)
-    (*   iIntros "_". *)
-    (*   iApply wp_value; auto. iIntros; discriminate. } *)
-    (* { incrementLPC_inv; simplify_map_eq. *)
-    (*   iApply wp_pure_step_later; auto. iMod ("Hcls" with "[HP Ha]");[iExists lw;iFrame|iModIntro]. iNext. *)
-    (*   iIntros "_". *)
-    (*   assert (dst <> PC) as HdstPC by (intros ->; by simplify_map_eq). *)
-    (*   simplify_map_eq. *)
-    (*   iApply ("IH" $! (<[dst := _]> (<[PC := _]> lregs)) with "[%] [] [Hmap] [$Hown]"); *)
-    (*     try iClear "IH"; eauto. *)
-    (*   { intro. cbn. by repeat (rewrite lookup_insert_is_Some'; right). } *)
-    (*   iIntros (ri v Hri Hsv). *)
-    (*   rewrite insert_commute // lookup_insert_ne // in Hsv; []. *)
-    (*   destruct (decide (ri = dst)); simplify_map_eq. *)
-    (*   { repeat rewrite fixpoint_interp1_eq; auto. } *)
-    (*   { by iApply "Hreg". } rewrite !fixpoint_interp1_eq /=. destruct Hp as [-> | ->];iFrame "Hinv". } *)
-  Admitted.
+    iIntros "!>" (regs' retv). iDestruct 1 as (HSpec) "[Ha Hmap]".
+    destruct HSpec; cycle 1.
+    { iApply wp_pure_step_later; auto. iMod ("Hcls" with "[HP Ha]");[iExists lw;iFrame|iModIntro]. iNext.
+      iIntros "_".
+      iApply wp_value; auto. iIntros; discriminate. }
+    { incrementLPC_inv; simplify_map_eq.
+      iApply wp_pure_step_later; auto. iMod ("Hcls" with "[HP Ha]");[iExists lw;iFrame|iModIntro]. iNext.
+      iIntros "_".
+      assert (dst <> PC) as HdstPC by (intros ->; by simplify_map_eq).
+      simplify_map_eq.
+      iApply ("IH" $! (<[dst := _]> (<[PC := _]> lregs)) with "[%] [] [Hmap] [$Hown]");
+        try iClear "IH"; eauto.
+      { intro. cbn. by repeat (rewrite lookup_insert_is_Some'; right). }
+      iIntros (ri v Hri Hsv).
+      rewrite insert_commute // lookup_insert_ne // in Hsv; [].
+      destruct (decide (ri = dst)); simplify_map_eq.
+      { repeat rewrite fixpoint_interp1_eq; auto. }
+      { by iApply "Hreg". } rewrite !fixpoint_interp1_eq /=. destruct Hp as [-> | ->];iFrame "Hinv". }
+  Qed.
 
 End fundamental.
